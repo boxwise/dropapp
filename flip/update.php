aa75548a5188dd1e7e8b51a12396465160486294
Bart Driessen <bart@wishingtree.eu>
2017-02-06 23:02:57 +0100

    bugfix

--- flip/update.php
+++ flip/update.php
@@ -7,8 +7,16 @@
 	  `name` varchar(255) DEFAULT NULL,
 	  `seq` int(11) NOT NULL,
 	  PRIMARY KEY (`id`)
-	) ENGINE=InnoDB DEFAULT CHARSET=latin1;');		
+	) ENGINE=InnoDB DEFAULT CHARSET=latin1;');	
+		db_query("INSERT INTO `camps` (`id`, `name`, `seq`) VALUES (1, 'Nea Kavala', 1), (2, 'Souda / Chios', 2);");
 	}
+	if(!db_tableexists('cms_users_camps')) {
+		db_query('CREATE TABLE `cms_users_camps` (
+  `cms_users_id` int(11) DEFAULT NULL,
+  `camps_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;');		
+	}
+
 
 	# add the last_update setting and set it to current timestamp (2017-02-06 BD)
 	if(!isset($settings['last_update'])) {
