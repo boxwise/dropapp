c55c02163432f816dc49a55ed69734843f1ec295
Maarten Hunink <maartenhunink@gmail.com>
2017-02-16 19:42:56 +0200

    Added a link to go back to the box after an edit

--- mobile/move.php
+++ mobile/move.php
@@ -9,4 +9,4 @@
 	db_query('UPDATE stock SET location_id = :location_id, modified = NOW() WHERE id = :id',array('location_id'=>$newlocation['id'],'id'=>$box['id']));
 	db_query('INSERT INTO history (tablename,record_id,changes,ip,changedate) VALUES ("stock",'.$box['id'].',"Box moved to '.$newlocation['label'].'","'.$_SERVER['REMOTE_ADDR'].'",NOW())');
 
-	redirect('?message='.'Box <strong>'.$box['box_id'].'</strong> contains '.$box['items'].'x <strong>'.$box['product'].'</strong> is moved from <strong>'.$box['location'].'</strong> to <strong>'.$newlocation['label']);
+	redirect('?message='.'Box <strong>'.$box['box_id'].'</strong> contains '.$box['items'].'x <strong>'.$box['product'].'</strong> is moved from <strong>'.$box['location'].'</strong> to <strong>'.$newlocation['label'].'</strong>. <a href="?boxid='.$box['id'].'">Go back to this box.</a>');
