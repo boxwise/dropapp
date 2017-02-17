cb20864dfe74121ee5593b723206226a3f4e90b0
Bart Driessen <post@bartdriessen.eu>
2017-02-17 12:42:31 +0200

    sorting in size dropdown is fixed

--- library/ajax/getsizes.php
+++ library/ajax/getsizes.php
@@ -4,7 +4,7 @@
 	$product_id = $_POST['product_id'];
 	$size_id = intval($_GET['size']);
 
-	$result = db_query('SELECT s.* FROM sizes AS s, sizegroup AS sg, products AS p WHERE s.sizegroup_id = sg.id AND p.id = :id AND p.sizegroup_id = sg.id',array('id'=>$product_id));
+	$result = db_query('SELECT s.* FROM sizes AS s, sizegroup AS sg, products AS p WHERE s.sizegroup_id = sg.id AND p.id = :id AND p.sizegroup_id = sg.id ORDER BY s.seq',array('id'=>$product_id));
 	$html = '<option></option>';
 	while($row = db_fetch($result)) {
 		$html .= '<option value="'.$row['id'].'" '.($size_id==$row['id']?'selected':'').'>'.$row['label'].'</option>';
