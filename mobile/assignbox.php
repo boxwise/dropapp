c8c57665a06c2451002079291780345336be3e75
Bart Driessen <post@bartdriessen.eu>
2017-02-19 13:15:22 +0200

    Product list is now per camp - updated

--- mobile/assignbox.php
+++ mobile/assignbox.php
@@ -6,7 +6,7 @@
 	LEFT OUTER JOIN genders AS g ON g.id = p.gender_id
 	LEFT OUTER JOIN sizes AS s2 ON s2.id = s.size_id
 	LEFT OUTER JOIN locations AS l ON l.id = s.location_id
-	WHERE NOT s.deleted AND l.visible AND (s.qr_id IS NULL OR s.qr_id = 0)
+	WHERE NOT s.deleted AND l.visible AND (s.qr_id IS NULL OR s.qr_id = 0) AND l.camp_id = '.$_SESSION['camp']['id'].'
 	ORDER BY s.box_id');
 
 	$tpl->assign('include','mobile_assign.tpl');
