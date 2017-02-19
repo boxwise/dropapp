a36d97cc4c3268e03cc2511ef1064596a5e5c8f2
Bart Driessen <post@bartdriessen.eu>
2017-02-19 17:16:21 +0200

    minor changes

--- include/products_edit.php
+++ include/products_edit.php
@@ -37,7 +37,7 @@
 	if($_SESSION['camp']['market']) addfield('number','Estimated annual need per person','amountneeded',array('width'=>3));
 	addfield('line','','');
 	$table = 'stock';
-	addfield('list','In Stock','stock', array('width'=>10,'query'=>'
+	if($id) addfield('list','In Stock','stock', array('width'=>10,'query'=>'
 	SELECT stock.id AS id, stock.box_id, stock.items, stock.comments, g.label AS gender, p.name AS product, p.name AS product, s.label AS size, l.label AS location, l.visible FROM '.$table.'
 	LEFT OUTER JOIN products AS p ON p.id = stock.product_id
 	LEFT OUTER JOIN locations AS l ON l.id = stock.location_id LEFT OUTER JOIN genders AS g ON g.id = p.gender_id LEFT OUTER JOIN sizes AS s ON s.id = stock.size_id WHERE stock.product_id = '.$id, 'columns'=>array('box_id'=>'Box ID', 'product'=>'Product', 'gender'=>'Gender', 'size'=>'Size','items'=>'Items', 'location'=>'Location', 'comments'=>'Comments'),
