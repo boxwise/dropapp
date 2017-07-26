bbe3f3c2c86840b841aaab7609b3716390a943f8
Maarten Hunink <maartenhunink@gmail.com>
2017-07-26 16:37:14 +0200

    fixed the permission denied error when checking connection with ajax

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -235,7 +235,7 @@ $('.checkConnectionOnSubmit').on('click', function(ev){
 	ev.preventDefault();
 	$.ajax({
 		type: 'post',
-		url: 'library/ajax/checkconnection.php',
+		url: 'ajax.php?file=checkconnection',
 		success: function(){
 			$('<input />').attr('type', 'hidden').attr('name', $(this).attr('name')).attr('value', $(this).attr('value')).appendTo('#cms_form');
 			$('#cms_form').submit();
--- library/ajax/checkconnection.php
+++ library/ajax/checkconnection.php
@@ -2,5 +2,5 @@
 	require_once('core.php');
 	$ajax = checkajax();
 	if($ajax) {
-		echo false;
+		echo true;
 	}
