0c3d9befb1c7d59bbec209594ea015fba0a21ecf
Bart Driessen <post@bartdriessen.eu>
2017-08-23 15:15:52 +0200

    making changes to the core, to remove the include_path

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 	error_reporting(E_ALL^E_NOTICE);
 	ini_set('display_errors',1);
 	
-
 	//die('Please use the online CMS!');
 	require_once($_SERVER["DOCUMENT_ROOT"].'/'.$settings['rootdir'].'library/core.php');
 
