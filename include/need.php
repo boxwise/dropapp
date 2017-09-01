fc4c779facc2bcea4fea42464b79c4768d7c1745
hpbaxxter <hape.guertner@gmx.de>
2017-09-01 17:34:10 +0200

    Needed items/General Stock merge finished for now

--- include/need.php
+++ include/need.php
@@ -63,95 +63,81 @@
  		listfilter2(array('label'=>'Scope','query'=>'SELECT id, label FROM need_periods ORDER BY week_min','filter'=>'scope'));
 
 
+			
+		$weeks = db_row('SELECT week_min AS min, week_max AS max FROM need_periods WHERE id = :id',array('id'=>$_SESSION['filter2']['need']));
+
 		//MYSQL query for products of the own camp
+		//calculation of need for the time periods in TOP QUERY, rest in BIG SUBQUERY 
 		$query = '
 		SELECT 
-			CONCAT(pro.id,"-",g.id,"-",IFNULL(s.id,"")) AS id,
-			1 AS visible, 
-			pro.name AS product,
-			g.label AS gender,
-			s.label AS size,
-			ROUND(pro.amountneeded,1) AS ean_print,
-			pro.amountneeded AS ean,
-			COALESCE(ROUND((SELECT COUNT(id) FROM people AS peo WHERE NOT peo.deleted AND peo.camp_id = '.$_SESSION['camp']['id'].' AND  
-			(IF(g.male,peo.gender="M",0) OR IF(g.female,peo.gender="F",0)) AND 
-			(IF(g.adult,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), peo.date_of_birth)), "%Y")+0>=13,0) OR IF(g.baby,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), peo.date_of_birth)), "%Y")+0<2,0) OR IF(g.child,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), peo.date_of_birth)), "%Y")+0 BETWEEN 2 AND 13,0)))*IFNULL(s.portion,100)/100),0) AS target,
-			COALESCE(SUM(st.items),0) AS stock,
-			COALESCE(COUNT(st.items),0) AS boxes';
-			
+			sub.*, 
+			ROUND((sub.target * sub.ean * '.$weeks['min'].'/52)- sub.stock) AS diff_min,
+			ROUND((sub.target * sub.ean * '.$weeks['max'].'/52)- sub.stock) AS diff_max,
+			IF((SELECT diff_min) >= 0, "red", IF((SELECT diff_max) < 0, "blue", "green")) AS color
+		FROM
+			(SELECT 
+				CONCAT(pro.id,"-",g.id,"-",IFNULL(s.id,"")) AS id,
+				1 AS visible, 
+				pro.name AS product,
+				g.label AS gender,
+				s.label AS size,
+				ROUND(pro.amountneeded,1) AS ean_print,
+				pro.amountneeded AS ean,
+				COALESCE(ROUND((SELECT COUNT(id) FROM people AS peo WHERE NOT peo.deleted AND peo.camp_id = '.$_SESSION['camp']['id'].' AND  
+				(IF(g.male,peo.gender="M",0) OR IF(g.female,peo.gender="F",0)) AND 
+				(IF(g.adult,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), peo.date_of_birth)), "%Y")+0>=13,0) OR IF(g.baby,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), peo.date_of_birth)), "%Y")+0<2,0) OR IF(g.child,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), peo.date_of_birth)), "%Y")+0 BETWEEN 2 AND 13,0)))*IFNULL(s.portion,100)/100),0) AS target,
+				COALESCE(SUM(st.items),0) AS stock,
+				COALESCE(COUNT(st.items),0) AS boxes';
+				
 		foreach($camps as $key=>$camp) {
 			$query .= ', st_'.$key.'.calc AS camp_'.$key;
 		}
 		
 		$query .= '
-		FROM 	
-			(sizegroup AS sg,
-			genders AS g,
-			sizes AS s,
-			products AS pro)
-		LEFT JOIN 
-			(SELECT st.items, st.product_id, st.size_id, s.label
-				FROM stock AS st, locations AS l, sizes AS s
-				WHERE NOT st.deleted AND st.location_id = l.id AND l.visible AND st.size_id = s.id) st  
-			ON (st.product_id = pro.id AND UPPER(st.label) = UPPER(s.label))';
+			FROM 	
+				(sizegroup AS sg,
+				genders AS g,
+				sizes AS s,
+				products AS pro)
+			LEFT JOIN 
+				(SELECT st.items, st.product_id, st.size_id, s.label
+					FROM stock AS st, locations AS l, sizes AS s
+					WHERE NOT st.deleted AND st.location_id = l.id AND l.visible AND st.size_id = s.id) st  
+				ON (st.product_id = pro.id AND UPPER(st.label) = UPPER(s.label))';
 		//left joins for other camps to distinguish between NULL and 0
 		foreach($camps as $key=>$camp) {
 			$query .= '
-		LEFT JOIN
-			(SELECT COALESCE(SUM(st.items),0) AS calc, UPPER(pro.name) AS product, pro.gender_id, UPPER(s.label) AS size
-				FROM (products AS pro, sizes AS s, locations AS l)
-				LEFT JOIN stock st
-				ON (NOT st.deleted AND st.location_id = l.id AND pro.id = st.product_id AND s.id = st.size_id)
-				WHERE l.visible AND l.camp_id = '.$key.' AND pro.camp_id = '.$key.'
-				GROUP BY UPPER(pro.name), pro.gender_id, UPPER(s.label)) st_'.$key.'
-			ON (st_'.$key.'.product = UPPER(pro.name) AND st_'.$key.'.gender_id = g.id AND st_'.$key.'.size = UPPER(s.label))';
+			LEFT JOIN
+				(SELECT COALESCE(SUM(st.items),0) AS calc, UPPER(pro.name) AS product, pro.gender_id, UPPER(s.label) AS size
+					FROM (products AS pro, sizes AS s, locations AS l)
+					LEFT JOIN stock st
+					ON (NOT st.deleted AND st.location_id = l.id AND pro.id = st.product_id AND s.id = st.size_id)
+					WHERE l.visible AND l.camp_id = '.$key.' AND pro.camp_id = '.$key.'
+					GROUP BY UPPER(pro.name), pro.gender_id, UPPER(s.label)) st_'.$key.'
+				ON (st_'.$key.'.product = UPPER(pro.name) AND st_'.$key.'.gender_id = g.id AND st_'.$key.'.size = UPPER(s.label))';
 		}
 		$query .='
-		WHERE
-			NOT pro.deleted AND
-			pro.camp_id = '.$_SESSION['camp']['id'].' AND
-			pro.gender_id = g.id AND
-			pro.sizegroup_id = sg.id AND
-			s.sizegroup_id = sg.id';
-
-		switch($_SESSION['filter']['need']) {
-			case 'red':
-				$query .= ' ';
-				break;
-			case 'green':
-				$query .= ' ';
-				break;
-			case 'blue':
-				$query .= ' ';
-				break;
-		}
+			WHERE
+				NOT pro.deleted AND
+				pro.camp_id = '.$_SESSION['camp']['id'].' AND
+				pro.gender_id = g.id AND
+				pro.sizegroup_id = sg.id AND
+				s.sizegroup_id = sg.id';
 
 		($listconfig['searchvalue']?$query .=' AND (pro.name LIKE "%'.$listconfig['searchvalue'].'%" OR g.label LIKE "%'.$listconfig['searchvalue'].'%" OR s.label LIKE "%'.$listconfig['searchvalue'].'%")':'').
-
 		$query .= ' GROUP BY 
-			pro.name, g.id, s.label
-		ORDER BY 
-			pro.name, g.seq, s.seq';
-		
+				pro.name, g.id, s.label
+			ORDER BY 
+				pro.name, g.seq, s.seq) AS sub ';
+		//end of big subquery, HAVING for filter of calculation
+		($_SESSION['filter']['need'] ? $query .= 'HAVING color = "'.$_SESSION['filter']['need'].'" ' : "") ;
+
 		//get visible data from camp
 		$data = getlistdata($query);
 		
 		foreach($data as $key=>$d) {
 			//href for ean_print to easily change "estimated annual need"
 			$data[$key]['ean_print'] = '<a href="?action=products_edit&origin='.$action.'&id='.$data[$key]['id'].'" class="tooltip-this" title="Click here to edit the estimated annual need">'.$d['ean_print'].'</a>';
-
-			$weeks = db_row('SELECT week_min AS min, week_max AS max FROM need_periods WHERE id = :id',array('id'=>$_SESSION['filter2']['need']));
-			
-			//calculation of need for the time periods 
-			$data[$key]['diff_min'] = round(($d['target']*$d['ean']*$weeks["min"]/52)-$d['stock']);
-			$data[$key]['diff_max'] = round(($d['target']*$d['ean']*$weeks["max"]/52)-$d['stock']);
-			//decision if items should be ordered or dropped
-			if($data[$key]['diff_min'] >= 0) {$data[$key]['color'] = "red";}
-			else if($data[$key]['diff_max'] < 0) {$data[$key]['color'] = "blue";} 
-			else {
-				$data[$key]['color'] = "green";
-				$data[$key]['diff_max'] = "";
-			}			
 		}
 
 		//products of other camps hidden
