53265a85336067f2c9074bebdbd32b09fb39a4bd
Maarten Hunink <maartenhunink@gmail.com>
2017-09-10 12:03:24 +0200

    fixed login when installed in directory

--- include/cms_login.php
+++ include/cms_login.php
@@ -1,7 +1,7 @@
 <?php
 setlocale(LC_ALL, 'nl_NL');
-require_once($_SERVER["DOCUMENT_ROOT"].'/'.$settings['rootdir'].'library/core.php');
+require_once('library/core.php');
 
-//$email = (isset($_COOKIE['saveemail']) ? $_COOKIE['saveemail']:'');
+$email = (isset($_COOKIE['saveemail']) ? $_COOKIE['saveemail']:'');
 $smarty = new Zmarty;
 $smarty->display('cms_login.tpl');
