f5053a7f7e8dd9388e1c8a5dfc9dfb553e00b2e3
hpbaxxter <hape.guertner@gmx.de>
2017-10-30 19:38:28 +0100

    New menu to print lists

---
+++ include/food_lists.php
@@ -0,0 +1,41 @@
+<?php
+
+	$action = 'food_lists';
+
+	if($_POST) {
+		$type = $_POST['type'][0];
+		if($type =='veg') {
+
+			redirect('?action=printed_list_people');
+
+		} else if ($type=='dry') {
+				
+			redirect('?action=food-distribution');
+
+		} else {
+
+			redirect('?action=printed_list_containers');
+
+		}
+
+	} else {
+		addfield('custom','','<div class="noprint tipofday"><h3>💡 Best printing result</h3><p>Print it out using Google Chrome, choose A4, Portrait and switch off the printing of headers and footers in the Print dialog screen.</p></div>');
+		addfield('line');
+		addfield('select', 'Which List', 'type', array('options'=>array(
+			array('value'=>'veg', 'label'=>'Vegetables'),
+			array('value'=>'dry', 'label'=>'Dry Food'),
+			array('value'=>'con', 'label'=>'Container'))));
+
+		// open the template
+		$cmsmain->assign('title','Sales overview');
+		$cmsmain->assign('include','cms_form.tpl');
+		// Title
+		// Form Button
+		$translate['cms_form_submit'] = 'Create list';
+		$cmsmain->assign('translate',$translate);
+		// place the form elements and data in the template
+		$cmsmain->assign('data',$data);
+		$cmsmain->assign('formelements',$formdata);
+		$cmsmain->assign('formbuttons',$formbuttons);
+
+	}
