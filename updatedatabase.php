15dd82850562778e2a7ff60ef53c888b0689747d
Bart Driessen <post@bartdriessen.eu>
2017-11-11 12:53:05 +0200

    bicycles

--- updatedatabase.php
+++ updatedatabase.php
@@ -119,7 +119,7 @@
 	
 	if(!db_tableexists('bicycles')) {
 		echo "Created table 'bicycles'<br />";
-		db_query("CREATE TABLE `bicycles` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT, `name` varchar(255) DEFAULT NULL, `deleted` tinyint(4) NOT NULL DEFAULT '0', PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8; LOCK TABLES `bicycles` WRITE; INSERT INTO `bicycles` (`id`, `name`, `deleted`) VALUES (1,'Bike 1',0), (2,'Bike 2',0), (3,'Bike 3',0), (4,'Bike 4',0), (5,'Bike 5',0), (6,'Bike 6',0), (7,'Bike 7',0), (8,'Bike 8',0), (9,'Bike 9',0), (10,'Bike 10',0), (11,'Bike 11',0), (12,'Bike 12',0), (13,'Bike 13',0), (14,'Bike 14',0), (15,'Bike 15',0), (16,'Bike 16',0), (17,'Bike 17',0), (18,'Bike 18',0), (19,'Bike 19',0), (20,'Bike 20',0); UNLOCK TABLES;");
+		db_query("CREATE TABLE `bicycles` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT, `label` varchar(255) DEFAULT NULL, `deleted` tinyint(4) NOT NULL DEFAULT '0', PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8; LOCK TABLES `bicycles` WRITE; INSERT INTO `bicycles` (`id`, `label`, `deleted`) VALUES (1,'Bike 1',0), (2,'Bike 2',0), (3,'Bike 3',0), (4,'Bike 4',0), (5,'Bike 5',0), (6,'Bike 6',0), (7,'Bike 7',0), (8,'Bike 8',0), (9,'Bike 9',0), (10,'Bike 10',0), (11,'Bike 11',0), (12,'Bike 12',0), (13,'Bike 13',0), (14,'Bike 14',0), (15,'Bike 15',0), (16,'Bike 16',0), (17,'Bike 17',0), (18,'Bike 18',0), (19,'Bike 19',0), (20,'Bike 20',0); UNLOCK TABLES;");
 	} else {
 		echo "Table 'bicycles' already exists<br />";
 	}
