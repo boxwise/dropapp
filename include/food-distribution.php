d6af918aab992224058f87326501a7634c508f18
Hans Peter Gürtner <hape.guertner@gmx.de>
2017-11-23 07:31:18 +0000

    food-distribution bug fix

--- include/food-distribution.php
+++ include/food-distribution.php
@@ -6,9 +6,9 @@
 
 	$result = db_query('
 		SELECT id, people.container, COUNT(*) AS number, SUM(extraportion) AS extra, 
-			SUM(IF((DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), "%Y")+0) >= '.$settings['adult-age'].', 1, 0)) AS adults, 
-			SUM(IF((DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), "%Y")+0) <= 2, 1, 0)) AS baby, 
-			SUM(IF((DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), "%Y")+0) >= '.$settings['adult-age'].' OR (DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), "%Y")+0) <= 2, 0, 1)) AS children
+			SUM(IF((DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), "%Y")+0) < '.$settings['adult-age'].', 0, 1)) AS adults, 
+			SUM(IF((DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), "%Y")+0) < 3, 1, 0)) AS baby, 
+			SUM(IF((DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), "%Y")+0) < '.$settings['adult-age'].' AND NOT (DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), "%Y")+0) < 3, 1, 0)) AS children
 		FROM people 
 		WHERE visible AND camp_id = '.$_SESSION['camp']['id'].' AND NOT deleted 
 		GROUP BY container 
@@ -28,14 +28,26 @@
 		$row['type'] = 'container';
 		$result2 = db_query('SELECT id, name FROM food WHERE visible AND NOT deleted AND (peradult > 0 OR perchild > 0) ORDER BY name');
 		while($row2 = db_fetch($result2)) {
-			$row['food'][$row2['name']] = db_value('SELECT CEIL(((SELECT count(id) FROM people AS p2 WHERE visible AND camp_id = '.$_SESSION['camp']['id'].' AND NOT deleted AND p2.container = p.container AND DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), p2.date_of_birth)), "%Y")+0 >= '.$settings['adult-age'].')*f.peradult + ((SELECT count(id) FROM people AS p2 WHERE visible AND NOT deleted AND p2.container = p.container AND DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), p2.date_of_birth)), "%Y")+0 < '.$settings['adult-age'].')*f.perchild))/f.package) AS rounded FROM people AS p LEFT OUTER JOIN food AS f ON f.id = '.$row2['id'].' WHERE p.visible AND container = "'.$row['container'].'" AND NOT p.deleted GROUP BY container ');
+			$row['food'][$row2['name']] = db_value('SELECT CEIL(((SELECT count(id) 
+					FROM people AS p2 
+					WHERE visible AND camp_id = '.$_SESSION['camp']['id'].' AND NOT deleted AND p2.container = p.container AND DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), p2.date_of_birth)), "%Y")+0 >= '.$settings['adult-age'].')*f.peradult + 
+					((SELECT count(id) 
+					FROM people AS p2 
+					WHERE visible AND NOT deleted AND p2.container = p.container AND DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), p2.date_of_birth)), "%Y")+0 < '.$settings['adult-age'].')*f.perchild))/f.package) AS rounded 
+				FROM people AS p 
+				LEFT OUTER JOIN food AS f ON f.id = '.$row2['id'].' 
+				WHERE p.visible AND container = "'.$row['container'].'" AND NOT p.deleted 
+				GROUP BY container ');
 		}
 		$list[] = $row;
 
-		$result2 = db_query('SELECT p.parent_id, CONCAT(TRIM(p.lastname),", ",TRIM(p.firstname)) AS name, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), "%Y")+0 AS age, IF(gender="M","Male","Female") AS gender, extraportion AS extra FROM people AS p WHERE visible AND camp_id = '.$_SESSION['camp']['id'].' AND NOT deleted AND container = "'.$row['container'].'" ORDER BY parent_id, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), "%Y")+0 DESC');
+		$result2 = db_query('SELECT p.parent_id, CONCAT(TRIM(p.lastname),", ",TRIM(p.firstname)) AS name, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), "%Y")+0 AS age, IF(gender="M","Male","Female") AS gender, extraportion AS extra 
+			FROM people AS p 
+			WHERE visible AND camp_id = '.$_SESSION['camp']['id'].' AND NOT deleted AND container = "'.$row['container'].'" 
+			ORDER BY parent_id, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), "%Y")+0 DESC');
 		while($row2 = db_fetch($result2)) {
 			$row2['type'] = ($row2['parent_id'])?'member':'familyhead';
-			if(!$row2['age']) $row2['age'] = '?';
+			#if(!$row2['age']) $row2['age'] = '?';
 			$list[] = $row2;
 		}
 	}
