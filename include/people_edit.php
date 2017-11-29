d6927704c5c6385db549c1964989feec6fe6b513
Bart Driessen <post@bartdriessen.eu>
2017-11-29 14:35:44 +0200

    Bicycles bug fix

--- include/people_edit.php
+++ include/people_edit.php
@@ -158,7 +158,7 @@
 			}
 			if(db_value('SELECT id FROM borrow_transactions WHERE people_id ='.$id)) {	
 				addfield('list','Bicycles','bicycles', array('width'=>10,'query'=>'
-					SELECT DATE_FORMAT(transaction_date,"%d-%m-%Y %H:%i") AS transaction_date, b.label, status  FROM borrow_transactions AS bt LEFT OUTER JOIN borrow_item AS b ON bt.bicycle_id = b.id WHERE people_id = '.$id.' ORDER BY transaction_date DESC LIMIT 10', 
+					SELECT DATE_FORMAT(transaction_date,"%d-%m-%Y %H:%i") AS transaction_date, b.label, status  FROM borrow_transactions AS bt LEFT OUTER JOIN borrow_items AS b ON bt.bicycle_id = b.id WHERE people_id = '.$id.' ORDER BY transaction_date DESC LIMIT 10', 
 					'columns'=>array('label'=>'Bicycle', 'status'=>'in/out', 'transaction_date'=>'Date'),
 					'allowedit'=>false,'allowadd'=>false,'allowsort'=>false,'allowselect'=>false,'allowselectall'=>false,'redirect'=>false,'modal'=>false));
 			}			
