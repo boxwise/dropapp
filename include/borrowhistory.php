701689ed96b0b1fe442c057923d4d09000286c59
Bart Driessen <post@bartdriessen.eu>
2018-01-13 17:31:36 +0200

    Bicycle history

---
+++ include/borrowhistory.php
@@ -0,0 +1,42 @@
+<?php
+
+	$table = 'borrow_transactions';
+	$ajax = checkajax();
+	if(!DEFINED('CORE')) include('core.php');
+
+
+	if(!$ajax) {
+
+		initlist();
+
+		$cmsmain->assign('title','History for '.db_value('SELECT label FROM borrow_items WHERE id = :id',array('id'=>$_GET['id'])));
+
+		$data = getlistdata('SELECT transaction_date AS dateout, 
+(SELECT transaction_date FROM borrow_transactions AS t2 WHERE t.bicycle_id = t2.bicycle_id AND t2.transaction_date > t.transaction_date ORDER BY transaction_date LIMIT 1) AS datein, CONCAT(p.firstname," ",p.lastname) AS name FROM borrow_transactions AS t LEFT OUTER JOIN people AS p ON p.id = t.people_id WHERE bicycle_id = '.intval($_GET['id']).' AND status = "out" ORDER BY transaction_date DESC');
+
+		addcolumn('text','Date out','dateout');
+		addcolumn('text','Date back','datein');
+		addcolumn('text','Rented out to','name');
+		
+		listsetting('allowedit', false);
+		listsetting('allowsort', true);
+		listsetting('allowdelete', false);
+		listsetting('allowshowhide', false);
+		listsetting('allowadd', false);
+		listsetting('allowselect', false);
+		listsetting('allowselectall', false);
+
+		$cmsmain->assign('data',$data);
+		$cmsmain->assign('listconfig',$listconfig);
+		$cmsmain->assign('listdata',$listdata);
+		$cmsmain->assign('include','cms_list.tpl');
+
+	} else {
+		switch ($_POST['do']) {
+		}
+
+		$return = array("success" => $success, 'message'=> $message, 'redirect'=>$redirect);
+
+		echo json_encode($return);
+		die();
+	}
