750ee60bf2d17695cbd22c79aaf41e4d873d6b30
Bart Driessen <post@bartdriessen.eu>
2018-03-26 15:40:14 +0200

    time zone issue

--- test.php
+++ test.php
@@ -2,8 +2,8 @@
 	require('library/core.php');
 	
 		date_default_timezone_set('Europe/Athens');
-	db_query('SET time_zone = "+02:00"');
+	db_query('SET time_zone = "+03:00"');
 
-echo db_row('SELECT NOW()');
+echo db_value('SELECT NOW()');
 
 echo strftime("%Y-%m-%d %H:%M:%S");
