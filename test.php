fff3497cf8121084510273aaa1496c27df29b196
Bart Driessen <post@bartdriessen.eu>
2018-03-26 15:36:23 +0200

    test

--- test.php
+++ test.php
@@ -1,4 +1,9 @@
 <?php
 	require('library/core.php');
 	
-echo strftime('%H:%M');
+		date_default_timezone_set('Europe/Athens');
+	db_query('SET time_zone = "+02:00"');
+
+echo db_row('SELECT NOW()');
+
+echo strftime("%Y-%m-%d %H:%M:%S");
