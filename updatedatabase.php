a96da72fe336efdc88a3349f1c4f159afdd5c570
Bart Driessen <post@bartdriessen.eu>
2018-03-28 14:41:29 +0300

    laundry

--- updatedatabase.php
+++ updatedatabase.php
@@ -55,7 +55,6 @@
 				for($time=1;$time<=5;$time++) {
 					for($machine=2;$machine<=6;$machine++) {
 						$x++;
-						echo $day.' '.$time.' '.$machine.' '.'<br />';
 						db_query('INSERT INTO laundry_slots (id, day, time, machine) VALUES (:id,:day,:time,:machine)',array('id'=>$x,'day'=>$day,'time'=>$time,'machine'=>$machine));
 					}
 	
