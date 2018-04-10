3e6e08f3c08fda0b959ba711b1bd2d38f4b2c07c
Bart Driessen <post@bartdriessen.eu>
2018-04-10 13:17:38 +0200

    Laundry stats bugfix

--- include/start.php
+++ include/start.php
@@ -86,14 +86,14 @@ FROM borrow_transactions AS b1 LEFT OUTER JOIN borrow_items AS i ON i.id = b1.bi
 		$data['laundry_appointments'] = db_value('SELECT COUNT(id) FROM laundry_appointments WHERE people_id > 0 AND cyclestart = :cyclestart',array('cyclestart'=>$settings['laundry_cyclestart']));
 		$data['laundry_slots'] = db_value('SELECT COUNT(id) FROM laundry_slots');
 		$data['laundry_noshow'] = db_value('SELECT COUNT(id) FROM laundry_appointments WHERE cyclestart = :cyclestart AND noshow',array('cyclestart'=>$settings['laundry_cyclestart']));
-		$result = db_query('SELECT (SELECT COUNT(id) FROM people WHERE id = people_id OR parent_id = people_id) AS c FROM laundry_appointments WHERE cyclestart = :cyclestart ', array('cyclestart'=>$settings['laundry_cyclestart']));
+		$result = db_query('SELECT (SELECT COUNT(id) FROM people WHERE id = people_id OR parent_id = people_id) AS c FROM laundry_appointments WHERE people_id > 0 AND cyclestart = :cyclestart GROUP BY people_id', array('cyclestart'=>$settings['laundry_cyclestart']));
 		while($row = db_fetch($result)) $data['laundry_beneficiaries'] += $row['c'];
 		
 		$previous = strftime('%Y-%m-%d',strtotime('-14 days',strtotime($settings['laundry_cyclestart'])));
 
 		$data['laundry_prev_appointments'] = db_value('SELECT COUNT(id) FROM laundry_appointments WHERE people_id > 0 AND cyclestart = :cyclestart',array('cyclestart'=>$previous));
 		$data['laundry_prev_noshow'] = db_value('SELECT COUNT(id) FROM laundry_appointments WHERE cyclestart = :cyclestart AND noshow',array('cyclestart'=>$previous));
-		$result = db_query('SELECT (SELECT COUNT(id) FROM people WHERE id = people_id OR parent_id = people_id) AS c FROM laundry_appointments WHERE cyclestart = :cyclestart ', array('cyclestart'=>$previous));
+		$result = db_query('SELECT (SELECT COUNT(id) FROM people WHERE id = people_id OR parent_id = people_id) AS c FROM laundry_appointments WHERE people_id > 0 AND cyclestart = :cyclestart GROUP BY people_id', array('cyclestart'=>$previous));
 		while($row = db_fetch($result)) $data['laundry_prev_beneficiaries'] += $row['c'];
 		
 
