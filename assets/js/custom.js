838a6d63875bea3fad2186027ebf821839fe81c2
Bart Driessen <post@bartdriessen.eu>
2018-05-28 10:45:39 +0300

    Bug fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -133,17 +133,8 @@ function updateLaundry(field, offset) {
 	}
 }
 
-function selectFamilyReload(field) {
-	value = $('#field_'+field).val();
-	if(value){	
-		
 
-	} else {
-		selectFamily(field);
-	}
-}
-
-function selectFamily(field){
+function selectFamily(field, reload){
 	value = $('#field_'+field).val();
 
 	var queryDict = {}
@@ -151,7 +142,7 @@ location.search.substr(1).split("&").forEach(function(item) {queryDict[item.spli
 
 	if(value){
 		
-		if(queryDict['people_id'] != value) window.location = '?action=check_out2&people_id='+value;
+		if(queryDict['people_id'] != value && reload) window.location = '?action=check_out2&people_id='+value;
 		
 		if(value != $('#div_purch').data('listid')){
 			$('#div_purch').hide();
--- include/check_in.php
+++ include/check_in.php
@@ -42,7 +42,7 @@
 		// put a title above the form
 		$cmsmain->assign('title','Check In');
 
-		addfield('select','Find '.$_SESSION['camp']['familyidentifier'],'people_id',array('onchange'=>'selectFamily("people_id")', 'required'=>true, 'multiple'=>false, 'query'=>'SELECT p.id AS value, CONCAT(p.container, " ",p.firstname, " ", p.lastname) AS label, NOT visible AS disabled FROM people AS p WHERE parent_id = 0 AND NOT p.deleted AND camp_id = '.$_SESSION['camp']['id'].' GROUP BY p.id ORDER BY SUBSTRING(REPLACE(container,"PK","Z"),1,1), SUBSTRING(REPLACE(container,"PK","Z"), 2, 10)*1'));
+		addfield('select','Find '.$_SESSION['camp']['familyidentifier'],'people_id',array('onchange'=>'selectFamily("people_id",false)', 'required'=>true, 'multiple'=>false, 'query'=>'SELECT p.id AS value, CONCAT(p.container, " ",p.firstname, " ", p.lastname) AS label, NOT visible AS disabled FROM people AS p WHERE parent_id = 0 AND NOT p.deleted AND camp_id = '.$_SESSION['camp']['id'].' GROUP BY p.id ORDER BY SUBSTRING(REPLACE(container,"PK","Z"),1,1), SUBSTRING(REPLACE(container,"PK","Z"), 2, 10)*1'));
 
 
 		addfield('ajaxstart','', '', array('aside'=>true, 'asidetop'=>true, 'id'=>'ajax-aside'));
