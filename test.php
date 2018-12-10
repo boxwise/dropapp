a4ca5d9ba16117360ef4474820d7bfd28696e978
Bart Driessen <post@bartdriessen.eu>
2018-12-10 17:15:12 +0100

    Reset counter in skaramagas

--- test.php
+++ test.php
@@ -1,25 +1,9 @@
 <?php
 	require('library/core.php');
 	
-	$result = db_query('SELECT 
-	p.*, SUM(t.drops) AS drops,
-	IF((SELECT COUNT(id) FROM people WHERE volunteer AND (id = p.id OR parent_id = p.id)),99999,dropcapadult * (SELECT COUNT(id) FROM people WHERE (id = p.id OR parent_id = p.id) AND DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),date_of_birth)),"%Y")+0 >= '.$settings['adult-age'].') +
-	dropcapchild * (SELECT COUNT(id) FROM people WHERE (id = p.id OR parent_id = p.id) AND DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),date_of_birth)),"%Y")+0 < '.$settings['adult-age'].')) AS maxdrops
-FROM 
-	people AS p, 
-	transactions AS t,
-	camps AS c 
-WHERE 
-	t.people_id = p.id AND
-	NOT p.deleted AND
-	c.id = p.camp_id AND 
-	p.parent_id = 0 
-GROUP BY p.id
-	');
-	
+	$result = db_query('SELECT * FROM people AS p WHERE camp_id = 3 AND parent_id = 0 AND NOT deleted');
 	while($row = db_fetch($result)) {
-		if($row['drops']>$row['maxdrops']) {
-			echo $row['firstname'].' '.$row['lastname'].' '.$row['container'].', '.$row['drops'].' drops capped to '.$row['maxdrops'].'<br />';
-			db_query('INSERT INTO transactions (people_id, description, drops, transaction_date) VALUES (:id, "Drops capped to maximum", :drops, NOW())',array('id'=>$row['id'],'drops'=>-$row['drops']+$row['maxdrops']));
-		}
+		$drops = db_value('SELECT SUM(drops) FROM transactions WHERE people_id = :people_id',array('people_id'=>$row['id']));
+		echo $row['id'].' '.$row['firstname'].' '.$row['lastname'].' '.$drops.'<br />';
+		db_query('INSERT INTO transactions (people_id, description, drops,transaction_date,user_id) VALUES (:people_id,"Manually reset to 0 drops",:drops,NOW(),1)',array('people_id'=>$row['id'],'drops'=>-$drops));
 	}
