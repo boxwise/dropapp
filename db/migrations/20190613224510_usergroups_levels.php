a23be9de7a78848874ee33d450b0ee8a663b6f3f
HaGuesto <hans@boxwise.co>
2019-06-14 00:45:53 +0200

    Another Usergroup Table

---
+++ db/migrations/20190613224510_usergroups_levels.php
@@ -0,0 +1,77 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class UsergroupsLevels extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('cms_usergroups', [
+                'id' => false,
+                'primary_key' => ['id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8',
+                'collation' => 'utf8_general_ci',
+                'comment' => '',
+                'row_format' => 'DYNAMIC',
+            ])
+            ->addColumn('userlevel', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'after' => 'allow_borrow_adddelete',
+            ])
+            ->save();
+        $this->table('cms_usergroups_levels', [
+                'id' => false,
+                'primary_key' => ['id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8',
+                'collation' => 'utf8_general_ci',
+                'comment' => '',
+                'row_format' => 'DYNAMIC',
+            ])
+            ->addColumn('id', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'signed' => false,
+                'identity' => 'enable',
+            ])
+            ->addColumn('level', 'integer', [
+                'null' => true,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'after' => 'id',
+            ])
+            ->addColumn('label', 'string', [
+                'null' => true,
+                'limit' => 255,
+                'collation' => 'utf8_general_ci',
+                'encoding' => 'utf8',
+                'after' => 'level',
+            ])
+            ->addColumn('shortlabel', 'string', [
+                'null' => true,
+                'limit' => 20,
+                'collation' => 'utf8_general_ci',
+                'encoding' => 'utf8',
+                'after' => 'label',
+            ])
+            ->create();
+        $this->table('laundry_stations', [
+                'id' => false,
+                'primary_key' => ['id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8',
+                'collation' => 'utf8_general_ci',
+                'comment' => '',
+                'row_format' => 'DYNAMIC',
+            ])
+            ->addColumn('camp_id', 'integer', [
+                'null' => true,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'after' => 'access',
+            ])
+            ->save();
+    }
+}
