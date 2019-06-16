e6f188ee4046e41adff2c0058744b8a695eb96b7
James Crowley <james.crowley@gmail.com>
2019-06-16 17:17:02 +0100

    Fix up cron job for reset

--- reset-demo.php
+++ reset-demo.php
@@ -1,8 +1,12 @@
 <?php
+    error_reporting(E_ALL^E_NOTICE);
+    ini_set('display_errors',1);
+
     require_once __DIR__.'/library/config.php';
     require_once __DIR__.'/vendor/autoload.php';
-    if (!$_SERVER['X-Appengine-Cron'])
-        return;
+    if (!array_key_exists('HTTP_X_APPENGINE_CRON',$_SERVER))
+         throw new Exception("Not called from AppEngine cron service");
+         
     use Phinx\Config\Config;
     use Phinx\Migration\Manager;
     use Symfony\Component\Console\Input\StringInput;
@@ -10,16 +14,15 @@
 
     $configArray = [
         'paths' => [
-            'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+            'seeds' => './db/seeds'
         ],
         'environments' => [
             'demo' => [
                 'adapter' => 'mysql',
-                'host' => $settings['db_host'],
+                'unix_socket' => $settings['db_socket'],
                 'name' => 'dropapp_demo',
                 'user' => $settings["db_user"],
-                'pass' => $settings["db_password"],
-                'port' => 3306,
+                'pass' => $settings["db_pass"],
                 'charset' => 'utf8',
                 'mysql_attr_init_command' => "SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO'",
             ]
@@ -27,6 +30,7 @@
     ];
     $config = new Config($configArray);
     $manager = new Manager($config, new StringInput(' '), new NullOutput());
-    echo "Resetting seed data\n";
+    echo("Resetting seed data");
     $manager->seed('demo');
-    echo "Reset";
+    echo("Successfully reset data");
+    
