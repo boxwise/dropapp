boxwise/dropapp

commit 5748aee1234735d95f8ab4ed6cedf070ea0c6227

Removing the last bits of settings and renaming the rest to Global Settings

mobile/login.php
@@ -4,13 +4,7 @@
 
 	$row = db_row('SELECT *, "org" AS usertype FROM cms_users WHERE email != "" AND email = :email AND (NOT deleted OR deleted IS NULL)',array('email'=>$_POST['email']));
 
-	if($row) { #e-mailaddress exists in database
-		if($settings['local_adminonly'] && !$row['is_admin'] && $_SERVER['Local']) {
-			$success = false;
-			$message = translate('cms_login_error_adminonly').$settings['local_adminonly'];
-			$redirect = false;
-			logfile('Mobile login on the local-website is blocked for '.$_POST['email']);
-		} elseif($row['pass']==$_POST['pass']) { # password is correct
+	if($row['pass']==$_POST['pass']) { # password is correct
 			$success = true;
 			$message = '';
 

include/cms_settings.php
@@ -1,6 +1,6 @@
 <?php
 
-	$table = 'settings';
+	$table = 'cms_settings';
 	$ajax = checkajax();
 
 	if(!$ajax) {
@@ -9,7 +9,6 @@
 
 		$cmsmain->assign('title',$translate['cms_settings']);
 
-		$hasCategory = db_fieldexists($table,'category_id');
 		$hasBilanguage = db_fieldexists($table,'description_nl');
 
 		if($hasBilanguage) {
@@ -18,14 +17,7 @@
 			listsetting('search', array('code','description'));
 		}
 
-		if($hasCategory) {
-			$hasSeq = db_fieldexists('settings_categories','seq');
-			listfilter(array('label'=>'Filter by category','query'=>'SELECT id AS value, name AS label FROM settings_categories '.(!$_SESSION['user']['is_admin']?' WHERE NOT admin_only':'').' '.($hasSeq?'ORDER BY seq':'ORDER BY id'),'filter'=>'category_id'));
-			$data = getlistdata('SELECT t.* FROM '.$table.' AS t LEFT OUTER JOIN settings_categories AS c ON t.category_id = c.id '.(!$_SESSION['user']['is_admin']?' WHERE NOT t.hidden AND (c.admin_only IS NULL OR NOT c.admin_only)':''));
-
-		} else {
-			$data = getlistdata('SELECT u.*, GROUP_CONCAT(c.name ORDER BY c.seq SEPARATOR ', ') AS camps FROM cms_users AS u INNER JOIN cms_users_camps AS x ON x.cms_users_id = u.id LEFT OUTER JOIN camps AS c ON c.id = x.camps_id '.($_SESSION['user']['is_admin']?'':' WHERE NOT t.hidden').' GROUP BY u.id');
-		}
+		$data = getlistdata('SELECT t.* FROM '.$table.' AS t');
 
 		if($hasBilanguage) {
 			addcolumn('text',$translate['cms_settings_description'],'description_'.$lan,array('width'=>'33%'));

include/cms_settings_edit.php
@@ -1,10 +1,9 @@
 <?php
-	$table = 'settings';
+	$table = 'cms_settings';
 
 	if($_POST) {
 
 		$hasBilanguage = db_fieldexists($table,'description_nl');
-		$hasCategory = db_fieldexists($table,'category_id');
 
 		//echo $_POST['value'];
 
@@ -12,11 +11,10 @@
 		//$handler->debug = true;
 
 		if($hasBilanguage) {
-			$keys = array('description_nl','description_en','code','value','hidden','type','options');
+			$keys = array('description_nl','description_en','code','value','type','options');
 		} else {
-			$keys = array('description','code','value','hidden','type','options');
+			$keys = array('description','code','value','type','options');
 		}
-		if($hasCategory) $keys[] = 'category_id';
 		$handler->savePost($keys);
 
 		if($_POST['__action']=='submitandedit') redirect('?action='.$action.'&origin='.$_POST['_origin'].'&id='.$id);
@@ -36,13 +34,8 @@
 	// define tabs
 
 	$hasBilanguage = db_fieldexists($table,'description_nl');
-	$hasCategory = db_fieldexists($table,'category_id');
 
 	if($_SESSION['user']['is_admin']) {
-
-		if($hasCategory) {
-			addfield('select',$translate['cms_settings_category'], 'category_id', array('query'=>'SELECT id AS value, name AS label FROM settings_categories'));
-		}
 
 		if($hasBilanguage) {
 			addfield('textarea',$translate['cms_settings_description'].' NL','description_nl');
@@ -63,7 +56,6 @@
 		// alleen voor type = select
 		addfield('text',$translate['cms_settings_options'],'options',array('tooltip'=>'Vul de mogelijke keuzes in als volgt: waarde=Label,waarde2=Label2,waarde3=Label3','hidden'=>$data['type']!='select'));
 
-		addfield('checkbox',$translate['cms_settings_hidden'],'hidden');
 		addfield('line');
 
 	} else {

library/core.php
@@ -20,7 +20,7 @@ if (array_key_exists('db_dsn',$settings)) {
 db_connect($db_dsn,$settings['db_user'],$settings['db_pass']);
 
 # get settings from settings table
-$result = db_query('SELECT code, value FROM settings');
+$result = db_query('SELECT code, value FROM cms_settings');
 while($row = db_fetch($result)) $settings[$row['code']] = $row['value'];
 
 $locale = db_row('SELECT locale FROM languages WHERE code = :lan',array('lan'=>$settings['cms_language']));

library/lib/tools.php
@@ -73,7 +73,7 @@ function ConvertURL(){
 		$_GET['get'.$count] = $item;
 		$count++;
 	}
-	if(!$lan) $lan = $settings['site_language'];
+	if(!$lan) $lan = $settings['cms_language'];
 }
 
 function redirect($url, $status = 301) {

library/lib/translate.php
@@ -10,7 +10,7 @@ if(defined('CORE')) {
 }
 
 if(defined('COREMOBILE')) {
-	$lan = $settings['site_language'];
+	$lan = $settings['cms_language'];
 	$lanid = db_value('SELECT id FROM languages WHERE code = :code',array('code'=>$lan));
 }
 

templates/cms_form_created.tpl
@@ -6,5 +6,5 @@
 	{$translate['cms_form_modifiedby']} {if $data['modified_by']}{$data['modified_by']}{else}<em>{$translate['cms_form_createdunknown']}</em>{/if}<br />{$translate['cms_form_ondate']} {$data['modified']} 
 {/if}
 
-	{if $settings['showhistory']}<br /> <br /><i class="fa fa-history"></i> <a href="{$settings['rootdir']}/ajax.php?file=history&table={if $table}{$table}{else}{$smarty.get.action|escape:'html'}{/if}&id={$data['id']}" class="fancybox" data-fancybox-type="ajax">{$translate['cms_form_view_modified']}</a>{/if}
+	<br /> <br /><i class="fa fa-history"></i> <a href="{$settings['rootdir']}/ajax.php?file=history&table={if $table}{$table}{else}{$smarty.get.action|escape:'html'}{/if}&id={$data['id']}" class="fancybox" data-fancybox-type="ajax">{$translate['cms_form_view_modified']}</a>
 </div>

commit 02776b78d38f4ff3c58a82782579993a897dfc5b

Trying to fix up mobile login syntax - broken in 5748aee I think

mobile/login.php
@@ -3,8 +3,8 @@
 	$_POST['pass'] = md5($_POST['pass']);
 
 	$row = db_row('SELECT *, "org" AS usertype FROM cms_users WHERE email != "" AND email = :email AND (NOT deleted OR deleted IS NULL)',array('email'=>$_POST['email']));
-
-	if($row['pass']==$_POST['pass']) { # password is correct
+	if($row) { #e-mailaddress exists in database
+		if($row['pass']==$_POST['pass']) { # password is correct
 			$success = true;
 			$message = '';
 
@@ -20,7 +20,6 @@
 				setcookie("autologin_user", null, time()-3600, '/');
 				setcookie("autologin_pass", null, time()-3600, '/');
 			}
-
 		} else { # password is not correct
 			$success = false;
 			$message = translate('cms_login_error_wrongpassword');