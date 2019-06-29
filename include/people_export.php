8cf0fc0b0061254a323e46074bf017d2119ee959
stephan <naphets123@gmail.com>
2019-06-29 22:50:09 +0700

    changed residents to beneficiaries

--- include/people_export.php
+++ include/people_export.php
@@ -1,7 +1,7 @@
 <?
 
 	header("Content-type: text/csv");
-	header("Content-Disposition: attachment; filename=All Residents ".$_SESSION['camp']['name'].".csv");
+	header("Content-Disposition: attachment; filename=All Benificiaries ".$_SESSION['camp']['name'].".csv");
 	header("Pragma: no-cache");
 	header("Expires: 0");
 
