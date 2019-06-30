9dad96a0c53ba993888d0abe6ab668cba661a579
HaGuesto <hans@boxwise.co>
2019-06-30 23:49:41 +0200

    Edit ajax.php (#34)
    * Edit ajax.php
    
    * Edit ajax.php
    
    * Edit ajax.php

--- reset.php
+++ reset.php
@@ -1,4 +1,9 @@
 <?php
-$login = true;
-require_once('library/core.php');
-require_once('include/cms_reset.php');
+	$login = true;
+	require_once('library/core.php');
+
+	if($_GET['hash']&&($_GET['userid']||$_GET['peopleid'])) {
+		require_once('include/cms_reset2.php');
+	} else {
+		require_once('include/cms_reset.php');
+	} 
