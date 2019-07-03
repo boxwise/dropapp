3af9aa16f33c683d89c71d78e5d94077bc73aa5e
HaGuesto <hans@boxwise.co>
2019-07-03 11:16:43 +0200

    Add Parent ID in product_categories

---
+++ db/migrations/20190703090715_add_parent_id_in_product_categories.php
@@ -0,0 +1,17 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddParentIdInProductCategories extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('product_categories')
+        ->addColumn('parent_id', 'integer', [
+            'signed' => false,
+            'null' => true,
+            'default' => NULL,
+        ])
+        ->save();
+    }
+}
