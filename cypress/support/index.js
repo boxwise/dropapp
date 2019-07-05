85f4f9cb98756ac77ec7b0e9d1bc7b268278c89c
DurkoMatko <nitramdurcek@gmail.com>
2019-07-05 16:46:36 +0200

    add: uncaught exception cypress handler

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,15 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    debugger;
+    return false;
+  })
+  
+  Cypress.on('fail', (err, runnable) => {
+      debugger;
+      return false;
+  })
