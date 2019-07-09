070e17ee1168a8b48fad7843321e29c9f828c677
HaGuesto <hans@boxwise.co>
2019-07-09 18:16:05 +0200

    Add naming of export columns

--- library/lib/csvexport.php
+++ library/lib/csvexport.php
@@ -1,19 +1,23 @@
 <?
 
-function csvexport($data,$filename,$keys){
+function csvexport($data, $filename, $keys)
+{
 
-header("Content-type: text/csv");
-header("Content-Disposition: attachment; filename=$filename".".csv");
-header("Pragma: no-cache");
-header("Expires: 0");
+    header("Content-type: text/csv");
+    header("Content-Disposition: attachment; filename=$filename" . ".csv");
+    header("Pragma: no-cache");
+    header("Expires: 0");
 
-foreach ($keys as $value){echo ucfirst($value).";";}
-echo "\n";
-
-while($f = db_fetch($data)) {
-    foreach($keys as $value){echo '"'.$f[$value].'"'.";";}
+    foreach ($keys as $key => $value) {
+        echo ucfirst($value) . ";";
+    }
     echo "\n";
 
-}
-die();
+    while ($f = db_fetch($data)) {
+        foreach ($keys as $key => $value) {
+            echo '"' . $f[$key] . '"' . ";";
+        }
+        echo "\n";
+    }
+    die();
 }
