5b63bb57c6c52ce80e1646e9ab0db06b38e74333
HaGuesto <hans@boxwise.co>
2019-07-09 18:15:26 +0200

    Clean products_export

--- include/products_export.php
+++ include/products_export.php
@@ -1,6 +1,7 @@
 <?
 $locations = join(',', db_simplearray('SELECT id, id FROM locations WHERE visible AND camp_id = :camp_id', array('camp_id' => $_SESSION['camp']['id'])));
-$result = db_query('SELECT p.*, sg.label AS sizegroup, g.label AS gender, CONCAT(p.value," ' . $_SESSION['camp']['currencyname'] . '") AS drops, COALESCE(SUM(s.items),0) AS items, IF(SUM(s.items),1,0) AS preventdelete, pc.label AS category FROM products AS p
+$result = db_query('SELECT p.*, sg.label AS sizegroup, g.label AS gender, p.value AS drops, COALESCE(SUM(s.items),0) AS items, pc.label AS category 
+		FROM products AS p
 		LEFT OUTER JOIN genders AS g ON g.id = p.gender_id
 		LEFT OUTER JOIN sizegroup AS sg ON sg.id = p.sizegroup_id
 		LEFT OUTER JOIN stock AS s ON s.product_id = p.id AND NOT s.deleted ' . ($locations ? ' AND s.location_id IN (' . $locations . ')' : '') . '
@@ -8,6 +9,6 @@ $result = db_query('SELECT p.*, sg.label AS sizegroup, g.label AS gender, CONCAT
 		WHERE (NOT p.deleted OR p.deleted IS NULL) AND camp_id = ' . $_SESSION['camp']['id'] . '
 		GROUP BY p.id ORDER BY category_id, g.seq, name
 	');
-$keys = array("name", "gender", "sizegroup", "items", "drops");
+$keys = array("name"=>"Product", "category"=>"Category", "gender"=>"Target Group", "sizegroup"=>"Sizegroup", "items" => "Items", "drops"=>$_SESSION['camp']['currencyname']);
 
 csvexport($result, "Products", $keys);
