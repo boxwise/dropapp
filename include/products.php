85fe84a9019e488da8cd220f52b90c2e5f2514a6
stephan <naphets123@gmail.com>
2019-07-15 15:13:11 +0700

    removed dump

--- include/products.php
+++ include/products.php
@@ -26,9 +26,7 @@
 		addcolumn('text','Product name','name');
 		addcolumn('text','Gender','gender');
 		addcolumn('text','Sizegroup','sizegroup');
-		dump($data['warehouse']);
 		if (db_value('SELECT id FROM locations WHERE locations.camp_id = '.intval($_SESSION['camp']['id']).' LIMIT 1 ')) addcolumn('text','Items','items');
-		dump(db_value('SELECT id FROM locations WHERE locations.camp_id = '.intval($_SESSION['camp']['id']).' LIMIT 1 '));
 		if(isset($checkwarehouse)) addcolumn('text','Items','items');
 		if($_SESSION['camp']['market']) addcolumn('text',ucfirst($_SESSION['camp']['currencyname']),'drops');
 		addcolumn('text','Description','comments');
