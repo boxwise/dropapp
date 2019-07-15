37462431e2a41fa8588327a5ab69bec04b4557c6
stephan <naphets123@gmail.com>
2019-07-15 16:03:05 +0700

    minor

--- include/products.php
+++ include/products.php
@@ -27,7 +27,6 @@
 		addcolumn('text','Gender','gender');
 		addcolumn('text','Sizegroup','sizegroup');
 		if (db_value('SELECT id FROM locations WHERE locations.camp_id = '.intval($_SESSION['camp']['id']).' LIMIT 1 ')) addcolumn('text','Items','items');
-		if(isset($checkwarehouse)) addcolumn('text','Items','items');
 		if($_SESSION['camp']['market']) addcolumn('text',ucfirst($_SESSION['camp']['currencyname']),'drops');
 		addcolumn('text','Description','comments');
 		if(db_value('SELECT id FROM locations WHERE locations.camp_id = '.intval($_SESSION['camp']['id']).' AND locations.container_stock ')){
