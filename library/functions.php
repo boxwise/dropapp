1551d985f319396d4d8b39985bd7d67e60d6473c
HaGuesto <hans@boxwise.co>
2019-07-15 16:08:00 +0200

    Fix camplist()

--- library/functions.php
+++ library/functions.php
@@ -8,11 +8,13 @@ function allowgivedrops()
 
 function camplist($short = false)
 {
+	$parameters = array('organisation_id' => $_SESSION['organisation']['id']);
+	if (!$_SESSION['user']['is_admin']) $parameters['usergroup_id'] = $_SESSION['usergroup']['id'];
 	$camplist = db_array('
 		SELECT c.* 
 		FROM camps AS c' . ($_SESSION['user']['is_admin']?'':', cms_usergroups_camps AS x').'
 		WHERE (NOT c.deleted OR c.deleted IS NULL) AND c.organisation_id = :organisation_id'. ($_SESSION['user']['is_admin']?'':' AND x.camp_id = c.id AND x.cms_usergroups_id = :usergroup_id').'
-		ORDER BY c.seq', array('organisation_id' => $_SESSION['organisation']['id'], 'usergroup_id'=> $_SESSION['usergroup']['id']), false, true);
+		ORDER BY c.seq', $parameters, false, true);
 	if ($short) {
 		foreach ($camplist as $c) $list[] = $c['id'];
 		return $list;
