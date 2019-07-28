ce2ee02c65d9c3f6277e84bdf034f703c86cf838
DurkoMatko <nitramdurcek@gmail.com>
2019-07-28 12:29:27 +0200

    add: initial shopping cart logic

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,162 @@
 const MIN_VALID_DATE = new Date('January 1, 1900 00:00:00');
 
 $(document).ready(function() {
+    var shoppingCart = (function() {
+        // =============================
+        // Private methods and propeties
+        // =============================
+        cart = [];
+        
+        // Constructor
+        function Item(id, name, count, price) {
+          this.id = id;
+          this.name = name;
+          this.price = price;
+          this.count = count;
+          debugger;
+        }
+        
+        // Save cart
+        function saveCart() {
+          sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+
+          $("#shopping_cart").find("tr:gt(0)").remove();
+          debugger;
+          cart.forEach((item) => {
+            $('#shopping_cart').append('<tr><td>'+item.name+'</td><td>'+item.count+'</td><td>'+item.price+'</td><td>delete</td></tr>');
+          });
+        }
+        
+          // Load cart
+        function loadCart() {
+          cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+        }
+        if (sessionStorage.getItem("shoppingCart") != null) {
+          loadCart();
+        }
+        
+
+        // =============================
+        // Public methods and propeties
+        // =============================
+        var obj = {};
+        
+        // Add to cart
+        obj.addItemToCart = function(id, name, price, count) {
+          for(var item in cart) {
+            if(cart[item].name === name) {
+              cart[item].count ++;
+              saveCart();
+              return;
+            }
+          }
+          var item = new Item(id, name, price, count);
+          cart.push(item);
+          saveCart();
+        }
+        // Set count from item
+        obj.setCountForItem = function(name, count) {
+          for(var i in cart) {
+            if (cart[i].name === name) {
+              cart[i].count = count;
+              break;
+            }
+          }
+        };
+        // Remove item from cart
+        obj.removeItemFromCart = function(name) {
+            for(var item in cart) {
+              if(cart[item].name === name) {
+                cart[item].count --;
+                if(cart[item].count === 0) {
+                  cart.splice(item, 1);
+                }
+                break;
+              }
+          }
+          saveCart();
+        }
+      
+        // Remove all items from cart
+        obj.removeItemFromCartAll = function(name) {
+          for(var item in cart) {
+            if(cart[item].name === name) {
+              cart.splice(item, 1);
+              break;
+            }
+          }
+          saveCart();
+        }
+      
+        // Clear cart
+        obj.clearCart = function() {
+          cart = [];
+          saveCart();
+        }
+      
+        // Count cart 
+        obj.totalCount = function() {
+          var totalCount = 0;
+          for(var item in cart) {
+            totalCount += cart[item].count;
+          }
+          return totalCount;
+        }
+      
+        // Total cart
+        obj.totalCart = function() {
+          var totalCart = 0;
+          for(var item in cart) {
+            totalCart += cart[item].price * cart[item].count;
+          }
+          return Number(totalCart.toFixed(2));
+        }
+      
+        // List cart
+        obj.listCart = function() {
+          var cartCopy = [];
+          for(i in cart) {
+            item = cart[i];
+            itemCopy = {};
+            for(p in item) {
+              itemCopy[p] = item[p];
+      
+            }
+            itemCopy.total = Number(item.price * item.count).toFixed(2);
+            cartCopy.push(itemCopy)
+          }
+          return cartCopy;
+        }
+      
+        // cart : Array
+        // Item : Object/Class
+        // addItemToCart : Function
+        // removeItemFromCart : Function
+        // removeItemFromCartAll : Function
+        // clearCart : Function
+        // countCart : Function
+        // totalCart : Function
+        // listCart : Function
+        // saveCart : Function
+        // loadCart : Function
+        return obj;
+    })();
+
+    $("#add-to-cart-button").click(function(e) {
+        e.preventDefault();
+        debugger;
+        var availableTokens = $("#dropcredit").data("dropCredit");
+        var count = $(this).closest("form").find("input[name='count']").val();
+        var productId = $("#field_product_id").val();
+        debugger;
+        var productName = $("#select2-chosen-2")[0].innerText;
+        var productvalue = $("#ajax-aside").data("productValue");
+        var totalprice = count * productvalue;
+        shoppingCart.addItemToCart(productId,productName, count, productvalue);
+    });
+});
+
+$(document).ready(function() {
     // Notification throught the redirect command
     var parts = window.location.search.substr(1).split("&");
     var $_GET = {};
@@ -331,6 +487,7 @@ function selectFamilyhead(field, targetfield) {
 }
 
 function getProductValue(field) {
+    debugger;
     value = $("#field_" + field).val();
     if (value) {
         $("#form-submit").prop("disabled", true);
@@ -379,9 +536,11 @@ function calcCosts(field) {
 
     if (dropcredit >= totalprice) {
         $("#form-submit").prop("disabled", false);
+        $("#add-to-cart-button").prop("disabled", false);
         $(".aside-form").removeClass("not_enough_coins");
     } else {
         $("#form-submit").prop("disabled", true);
+        $("#add-to-cart-button").prop("disabled", true);
         $(".aside-form").addClass("not_enough_coins");
     }
 }
