22a86ebd98e4631850fe1408de636db883a29fc5
DurkoMatko <nitramdurcek@gmail.com>
2019-07-30 20:04:49 +0200

    fix: cart styling and change quantity bugs, prevent cange quantity form submit

--- include/check_out.php
+++ include/check_out.php
@@ -53,7 +53,7 @@
 		addfield('select','Family/Beneficiary','people_id',array('onchange'=>'selectFamily("people_id",false)', 'required'=>true, 'multiple'=>false, 'query'=>'SELECT p.id AS value, CONCAT(p.container, " ",p.firstname, " ", p.lastname) AS label, NOT visible AS disabled FROM people AS p WHERE parent_id = 0 AND NOT p.deleted AND camp_id = '.$_SESSION['camp']['id'].' GROUP BY p.id ORDER BY SUBSTRING(REPLACE(container,"PK","Z"),1,1), SUBSTRING(REPLACE(container,"PK","Z"), 2, 10)*1'));
 		addfield('select','Product','product_id',array('onchange'=>'getProductValue("product_id");','required'=>true,'multiple'=>false,'query'=>'SELECT p.id AS value, CONCAT(p.name, " " ,IFNULL(g.label,""), " (",p.value," '.$_SESSION['camp']['currencyname'].')") AS label FROM products AS p LEFT OUTER JOIN genders AS g ON p.gender_id = g.id WHERE (NOT p.deleted OR p.deleted IS NULL) AND p.camp_id = '.$_SESSION['camp']['id'].' ORDER BY name'));
 		addfield('number', 'Number', 'count', array('onchange'=>"calcCosts('count')", 'onkeyup'=>"calcCosts('count')", 'required'=>true,'width'=>2));
-		addfield('custom','',"<p id='product_id_selected'>Price: <img src='../assets/img/more_coins.png' class='coinsImage'></img><span id='productvalue'>0</span> {$currency}</p>");
+		addfield('custom','',"<p id='selectedProductPrice'>Price: <img src='../assets/img/more_coins.png' class='coinsImage'></img><span id='productvalue_cart'>0</span> {$currency}</p>");
 		addfield('custom','','<button id="add-to-cart-button" type="button" class="btn btn-success">Add to cart</button>');
 		#addfield('text','Note','description');
 		addfield('line');
