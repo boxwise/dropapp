496a78c8641077496ba05a2171991099f2acc4f4
stephan <naphets123@gmail.com>
2019-08-04 16:39:48 +0200

    removed old variable assignments

--- include/people_edit.php
+++ include/people_edit.php
@@ -80,8 +80,6 @@
 		$ajaxaside = new Zmarty;
 
 		#$formdata = $formbuttons = '';
-		$side['approvalsigned'] = db_value('SELECT approvalsigned FROM people WHERE id = :id', array('id'=>$id));
-		$side['date_of_signature'] = db_value('SELECT date_of_signature FROM people WHERE id = :id', array('id'=>$id));
 		$side['approvalsigned'] = $side['date_of_signature'] = db_row('SELECT approvalsigned, date_of_signature FROM people WHERE id = :id', array('id'=>$id));
 		$side['allowdrops'] = allowGiveDrops();
 
