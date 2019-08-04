c257bdfaa7fba1415c0744269cadfd468639c391
James Crowley <james.crowley@gmail.com>
2019-08-04 15:47:58 +0000

    Fix tabs vs spaces

--- library/lib/smarty.php
+++ library/lib/smarty.php
@@ -2,8 +2,8 @@
 
 use OpenCensus\Trace\Tracer;
 class Zmarty extends Smarty {
-	public function __construct() {
-		global $settings, $translate, $lan;
+    public function __construct() {
+        global $settings, $translate, $lan;
 
         parent::__construct();
         if (parse_url($_SERVER['HTTP_HOST'], PHP_URL_HOST) != "localhost") {
@@ -12,11 +12,11 @@ class Zmarty extends Smarty {
             echo "disable";
         }
         $this->setCompileDir($settings['smarty_dir']);
-		$this->addTemplateDir($_SERVER['DOCUMENT_ROOT'].$settings['rootdir'].'/templates');
-		$this->assign('lan',$lan);
-		$this->assign('modal',isset($_GET['modal']));
-		$this->assign('settings',$settings);
-		$this->assign('translate',$translate);
+        $this->addTemplateDir($_SERVER['DOCUMENT_ROOT'].$settings['rootdir'].'/templates');
+        $this->assign('lan',$lan);
+        $this->assign('modal',isset($_GET['modal']));
+        $this->assign('settings',$settings);
+        $this->assign('translate',$translate);
     }
 
     public function display($template = NULL, $cache_id = NULL, $compile_id = NULL, $parent = NULL) {
