a10d51f6fec5e8445a169483d18c8dde0be2e40f
James Crowley <james.crowley@gmail.com>
2019-08-04 20:33:48 +0000

    Enable PHP warnings when running locally

--- library/error-reporting.php
+++ library/error-reporting.php
@@ -1,7 +1,12 @@
 <?php
 use OpenCensus\Trace\Tracer;
-// report errors, but ignore E_STRICT and E_NOTICE
-error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
+if (@parse_url($_SERVER['HTTP_HOST'], PHP_URL_HOST) == "localhost") {
+    // report all errors in local development
+    error_reporting(E_ALL);
+} else {
+    // report errors, but ignore E_STRICT and E_NOTICE
+    error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
+}
 
 // configure sentry
 if (!array_key_exists("sentry_key",$settings)) {
