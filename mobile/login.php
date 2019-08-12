a24f81cfcd046f6cd6a096b6829370f87372eb44
James Crowley <james.crowley@gmail.com>
2019-08-12 20:01:53 +0000

    Revert redirect to absolute but stay on HTTPS
    We were redirecting with two // which broke the relative redirects, and given we have a potential open redirect vulnerability here figured it was safer to stick with absolute

--- mobile/login.php
+++ mobile/login.php
@@ -3,8 +3,7 @@
 $login = login($_POST['email'], $_POST['pass'], $_POST['autologin'], $mobile = true);
 
 if ($login['success']) {
-	// WARNING: this is an open redirect, need to review the risk here
-	redirect('/' . $_POST['destination']);
+	redirect('http://' . $_SERVER['HTTP_HOST'] . $_POST['destination']);
 } else {
 	redirect('?warning=true&message=' . $login['message']);
 }
