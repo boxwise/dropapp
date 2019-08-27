8aeb313fb94a7fb4fb59df0d35ddf790f94485b9
James Crowley <james.crowley@gmail.com>
2019-08-27 10:34:17 +0100

    Revert "Enable PHP warnings when running locally"
    This reverts commit b2bb6bb126e0b4150851e17e6b9bdcb3a4e9598a.

--- library/error-reporting.php
+++ library/error-reporting.php
@@ -1,12 +1,7 @@
 <?php
 use OpenCensus\Trace\Tracer;
-if (@parse_url($_SERVER['HTTP_HOST'], PHP_URL_HOST) == "localhost") {
-    // report all errors in local development
-    error_reporting(E_ALL);
-} else {
-    // report errors, but ignore E_STRICT and E_NOTICE
-    error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
-}
+// report errors, but ignore E_STRICT and E_NOTICE
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
 
 // configure sentry
 if (!array_key_exists("sentry_key",$settings)) {
