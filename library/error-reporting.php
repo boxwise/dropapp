0129684e631adecc1ca44e571d498a6a263cc0c9
James Crowley <james.crowley@gmail.com>
2019-08-27 11:43:29 +0100

    Log errors to stderr rather than destroying UI
    Even when locally, there's so many errors that the redirects don't work in many places, so will have to make do with a lot of noise on the console for now instead

--- library/error-reporting.php
+++ library/error-reporting.php
@@ -1,15 +1,22 @@
 <?php
+
 use OpenCensus\Trace\Tracer;
+
 if (@parse_url($_SERVER['HTTP_HOST'], PHP_URL_HOST) == "localhost") {
     // report all errors in local development
     error_reporting(E_ALL);
+    // dump errors to stderr (you can see in console)
+    // this is so it doesn't interfere with the app interaction
+    ini_set('display_errors', 'stderr');
+    // as we're dumping to stderr, don't show the errors as html
+    ini_set('html_errors', '0');
 } else {
     // report errors, but ignore E_STRICT and E_NOTICE
     error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
 }
 
 // configure sentry
-if (!array_key_exists("sentry_key",$settings)) {
+if (!array_key_exists("sentry_key", $settings)) {
     throw new Exception("sentry_key must be set to work in GAE environment");
 }
 
@@ -18,10 +25,11 @@ Tracer::inSpan(
     function () {
         global $settings;
         Sentry\init(
-        [
-            'dsn' => $settings['sentry_key'],
-            'environment' => $_SERVER['HTTP_HOST'],
-            'error_types' => error_reporting()
-        ]);
+            [
+                'dsn' => $settings['sentry_key'],
+                'environment' => $_SERVER['HTTP_HOST'],
+                'error_types' => error_reporting()
+            ]
+        );
     }
 );
