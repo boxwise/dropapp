8286d065d6c2d0d114805cfde52f7442f15fc887
stephan <naphets123@gmail.com>
2019-09-09 22:35:20 +0200

    camps-migration

--- db/migrations/20190909173347_foreign_keys_camps.php
+++ db/migrations/20190909173347_foreign_keys_camps.php
@@ -4,59 +4,42 @@ use Phinx\Migration\AbstractMigration;
 
 class ForeignKeysCamps extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    addCustomColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Any other destructive changes will result in an error when trying to
-     * rollback the migration.
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
     public function change()
-    {
+    {/*
         $this->table('people')
-            ->changeColumn('camp_id', 'integer', ['signed' => false, 'default' => null])
+            ->changeColumn('camp_id', 'integer', [
+                'signed' => false,
+                'default' => null, ])
 
             ->addForeignKey('camp_id', 'camps', 'id', [
                 'delete' => 'RESTRICT', 'update' => 'CASCADE',
             ])
             ->save()
-    ;
+    ;*/
         $this->table('laundry_stations')
-            ->changeColumn('camp_id', 'integer', ['signed' => false, 'null' => false])
+            ->changeColumn('camp_id', 'integer', [
+                'signed' => false,
+                'null' => false, ])
 
             ->addForeignKey('camp_id', 'camps', 'id', [
                 'delete' => 'RESTRICT', 'update' => 'CASCADE',
             ])
             ->save()
-    ;
+    ; /*
         $this->table('locations')
-            ->changeColumn('camp_id', 'integer', ['signed' => false, 'null' => false])
+            ->changeColumn('camp_id', 'integer', [
+                'signed' => false,
+                'null' => false, ])
 
             ->addForeignKey('camp_id', 'camps', 'id', [
                 'delete' => 'RESTRICT', 'update' => 'CASCADE',
             ])
             ->save()
-    ;
+    ;*/
         $this->table('library_type')
-            ->changeColumn('camp_id', 'integer', ['signed' => false, 'default' => null])
+            ->changeColumn('camp_id', 'integer', [
+                'signed' => false,
+                'default' => null, ])
 
             ->addForeignKey('camp_id', 'camps', 'id', [
                 'delete' => 'RESTRICT', 'update' => 'CASCADE',
@@ -64,7 +47,9 @@ class ForeignKeysCamps extends AbstractMigration
             ->save()
     ;
         $this->table('library')
-            ->changeColumn('camp_id', 'integer', ['signed' => false, 'default' => null])
+            ->changeColumn('camp_id', 'integer', [
+                'signed' => false,
+                 'default' => null, ])
 
             ->addForeignKey('camp_id', 'camps', 'id', [
                 'delete' => 'RESTRICT', 'update' => 'CASCADE',
@@ -72,7 +57,9 @@ class ForeignKeysCamps extends AbstractMigration
             ->save()
     ;
         $this->table('borrow_locations')
-            ->changeColumn('camp_id', 'integer', ['signed' => false, 'null' => 'false'])
+            ->changeColumn('camp_id', 'integer', [
+                'signed' => false,
+                 'null' => false, ])
 
             ->addForeignKey('camp_id', 'camps', 'id', [
                 'delete' => 'RESTRICT', 'update' => 'CASCADE',
