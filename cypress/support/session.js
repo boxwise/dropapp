e14b2b575ed71ab41399820dc5b4ac0be14e1504
James Crowley <james.crowley@gmail.com>
2019-09-13 19:49:10 +0100

    Using explicit helpers for logging in with particular credentials
    This should make it simpler for test writing, so they don't have to deal with getting the config to read out the passwords

--- cypress/support/session.js
+++ cypress/support/session.js
@@ -1,13 +1,13 @@
-// Everything regarding SESSION data
+import { getLoginConfiguration } from '../config';
 
 // Standard Login method for all tests
-Cypress.Commands.add("LoginAjax", (userMail, userPassword, autologin) => {
+function loginWithAjax(userMail, userPassword) {
     cy.request({
         method: "POST",
         url: '/ajax.php?file=login',
         body: {
-            email: Cypress.env("testAdmin"),
-            pass: Cypress.env("testPwd")
+            email: userMail,
+            pass: userPassword
         },
         form: true
     }).then(response => {
@@ -15,29 +15,22 @@ Cypress.Commands.add("LoginAjax", (userMail, userPassword, autologin) => {
         expect(response.body.message).to.be.empty;
         expect(response.body.success).to.be.true;
     });
+};
+
+// these are shortcuts to sign in via ajax, as we
+// don't need to test the full login flow for most
+// tests
+Cypress.Commands.add("loginAsVolunteer", () => {
+    let config = getLoginConfiguration();
+    loginWithAjax(config.testVolunteer, config.testPwd);
 });
 
-// Login commands trhough login page
-Cypress.Commands.add("Login", (userMail, userPassword) => {
-    cy.visit("/login.php");
-    cy.get("input[data-testid='email']").type(`${userMail}`);
-    cy.get("input[data-testid='password']").type(`${userPassword}`);
-    cy.get("input[data-testid='signInButton']").click();
+Cypress.Commands.add("loginAsAdmin", () => {
+    let config = getLoginConfiguration();
+    loginWithAjax(config.testAdmin, config.testPwd);
 });
 
-Cypress.Commands.add("LoginMobile", (userMail, userPassword) => {
-    cy.visit("/mobile.php");
-    cy.get("input[data-testid='email']").type(`${userMail}`);
-    cy.get("input[data-testid='password']").type(`${userPassword}`);
-    cy.get("input[data-testid='signInButton']").click();
+Cypress.Commands.add("loginAsCoordinator", () => {
+    let config = getLoginConfiguration();
+    loginWithAjax(config.testCoordinator, config.testPwd);
 });
-
-// For Boxwise Gods to select organisation
-Cypress.Commands.add("SelectOrganisationByName", orgName => {
-    cy.get("a[data-testid='organisationsDropdown']").click();
-    cy.get("li[data-testid='organisationOption'] a")
-        .invoke("text")
-        .contains(orgName)
-        .click();
-});
-
