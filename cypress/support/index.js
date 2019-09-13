af6ab5b363570fe2ca7f996a9537e28b1e5c1328
DurkoMatko <nitramdurcek@gmail.com>
2019-09-13 21:42:07 +0200

    add: cypress helper methods to interact with select2 element

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,5 +1,6 @@
 import './session'
 import './notifications'
+import './select2-interaction-methods'
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
