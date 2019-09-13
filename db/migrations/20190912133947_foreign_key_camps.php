835227f75ea554a6651debeeb2007f325094e643
stephan <naphets123@gmail.com>
2019-09-13 17:24:08 +0200

    Reformatted

--- db/migrations/20190912133947_foreign_key_camps.php
+++ db/migrations/20190912133947_foreign_key_camps.php
@@ -32,78 +32,66 @@ class ForeignKeyCamps extends AbstractMigration
     public function change()
     {
         $this->table('people')
-
-        ->changeColumn('camp_id', 'integer', [
-            'signed' => false,
-            'default' => null,
-        ])
-
-        ->addForeignKey('camp_id', 'camps', 'id', [
-            'delete' => 'RESTRICT', 'update' => 'CASCADE',
-        ])
-        ->save()
+            ->changeColumn('camp_id', 'integer', [
+                'signed' => false,
+                'default' => null,
+            ])
+            ->addForeignKey('camp_id', 'camps', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])
+            ->save()
         ;
 
         $this->table('locations')
-
-        ->changeColumn('camp_id', 'integer', [
-            'null' => false,
-            'signed' => false,
-        ])
-
-        ->addForeignKey('camp_id', 'camps', 'id', [
-            'delete' => 'RESTRICT', 'update' => 'CASCADE',
-        ])
-        ->save()
+            ->changeColumn('camp_id', 'integer', [
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addForeignKey('camp_id', 'camps', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])
+            ->save()
         ;
         $this->table('laundry_stations')
-
-        ->changeColumn('camp_id', 'integer', [
-            'null' => false,
-            'signed' => false,
-        ])
-
-        ->addForeignKey('camp_id', 'camps', 'id', [
-            'delete' => 'RESTRICT', 'update' => 'CASCADE',
-        ])
-        ->save()
+            ->changeColumn('camp_id', 'integer', [
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addForeignKey('camp_id', 'camps', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])
+            ->save()
     ;
         $this->table('library_type')
-
-        ->changeColumn('camp_id', 'integer', [
-            'default' => null,
-            'signed' => false,
-        ])
-
-        ->addForeignKey('camp_id', 'camps', 'id', [
-            'delete' => 'RESTRICT', 'update' => 'CASCADE',
-        ])
-        ->save()
+            ->changeColumn('camp_id', 'integer', [
+                'default' => null,
+                'signed' => false,
+            ])
+            ->addForeignKey('camp_id', 'camps', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])
+            ->save()
         ;
         $this->table('library')
-
-        ->changeColumn('camp_id', 'integer', [
-            'default' => null,
-            'signed' => false,
-        ])
-
-        ->addForeignKey('camp_id', 'camps', 'id', [
-            'delete' => 'RESTRICT', 'update' => 'CASCADE',
-        ])
-        ->save()
+            ->changeColumn('camp_id', 'integer', [
+                'default' => null,
+                'signed' => false,
+            ])
+            ->addForeignKey('camp_id', 'camps', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])
+            ->save()
         ;
 
         $this->table('borrow_locations')
-
-        ->changeColumn('camp_id', 'integer', [
-            'null' => false,
-            'signed' => false,
-        ])
-
-        ->addForeignKey('camp_id', 'camps', 'id', [
-            'delete' => 'RESTRICT', 'update' => 'CASCADE',
-        ])
-        ->save()
+            ->changeColumn('camp_id', 'integer', [
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addForeignKey('camp_id', 'camps', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])
+            ->save()
         ;
     }
 }
