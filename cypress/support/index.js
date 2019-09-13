48c6dcf1789b39a36fe7819510b295ce2dd65c70
DurkoMatko <nitramdurcek@gmail.com>
2019-09-13 21:38:43 +0200

    Revert "add: cypress helper methods to interact with select2 element"
    This reverts commit 9171bea0b1fdd1fba93f7586691285f71f8b6df2.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,6 +1,5 @@
 import './session'
 import './notifications'
-import './select2-interaction-methods'
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
