cfa1042edae284e933c1a4a2dd9587460f7ed4f0
naphets123 <42490840+naphets123@users.noreply.github.com>
2019-09-14 11:59:05 +0200

    Reset password null (#177)
    Changed condition for reset to include NULL values, as some data has NULL rather than 0000-00-00 (which unfortunately we're historically using to indicate null in date-time fields...). Deciding just to fix the query rather than the data for now.

--- library/ajax/reset.php
+++ library/ajax/reset.php
@@ -2,7 +2,7 @@
 
 $row = db_row('SELECT *, "org" AS usertype FROM cms_users WHERE email != "" 
 	AND email = :email 
-	AND ((valid_firstday < NOW() AND valid_lastday > NOW()) OR UNIX_TIMESTAMP(valid_lastday) = 0)
+	AND ((valid_firstday < NOW() AND valid_lastday > NOW()) OR UNIX_TIMESTAMP(valid_lastday) = 0 OR valid_lastday IS NULL)
 	AND (NOT deleted OR deleted IS NULL)', ['email' => $_POST['email']]);
 
     if ($row) { //e-mailaddress exists in database
