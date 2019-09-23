8af779f4a16038f4afcb7e90e59aeb37c0534ff1
stephan <naphets123@gmail.com>
2019-09-23 12:44:27 +0200

    Added foreign keys for cms_users

---
+++ db/migrations/20190923101044_foreign_keys_users.php
@@ -0,0 +1,60 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class ForeignKeysUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $this->table('history')
+        ->changeColumn('user_id', 'integer', [
+            'null' => false,
+        ])
+        ->addForeignKey('user_id', 'cms_users', 'id', [
+            'delete' => 'RESTRICT', 'update' => 'CASCADE',
+        ])
+        ->save()
+    ;
+
+        $this->table('transactions')
+        ->addForeignKey('user_id', 'cms_users', 'id', [
+            'delete' => 'RESTRICT', 'update' => 'CASCADE',
+        ])
+        ->save()
+    ;
+        $this->table('log')
+        ->changeColumn('user', 'integer', [
+            'null' => false,
+        ])
+        ->addForeignKey('user', 'cms_users', 'id', [
+            'delete' => 'RESTRICT', 'update' => 'CASCADE',
+        ])
+        ->save()
+    ;
+    }
+}
