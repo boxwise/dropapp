b4439321eaef847322b48de9a6de7c2d5ed11452
James Crowley <james.crowley@gmail.com>
2019-10-02 18:15:15 +0100

    Allow the reseed to use full db configuration
    But only allow demo / staging

--- reseed-db.php
+++ reseed-db.php
@@ -2,6 +2,10 @@
     if (!array_key_exists('HTTP_X_APPENGINE_CRON', $_SERVER)) {
         throw new Exception('Not called from AppEngine cron service');
     }
+    $permittedDatabases = ['dropapp_demo', 'dropapp_staging'];
+    if (!in_array($settings['db_database'], permittedDatabases)) {
+        throw new Exception('Not permitting a reset of '.$settings['db_database']);
+    }
     use Phinx\Config\Config;
     use Phinx\Migration\Manager;
     use Symfony\Component\Console\Input\StringInput;
@@ -15,7 +19,7 @@
             'demo' => [
                 'adapter' => 'mysql',
                 'unix_socket' => $settings['db_socket'],
-                'name' => 'dropapp_demo',
+                'name' => $settings['db_database'],
                 'user' => $settings['db_user'],
                 'pass' => $settings['db_pass'],
                 'charset' => 'utf8',
