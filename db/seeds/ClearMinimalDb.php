4be2e79ac795eddc54300a2abdad8d2aede69a36
James Crowley <james.crowley@gmail.com>
2019-10-02 20:25:50 +0100

    Rejig deletion of seed data so it works with foreign key constraints

--- db/seeds/ClearMinimalDb.php
+++ db/seeds/ClearMinimalDb.php
@@ -16,7 +16,6 @@ class ClearMinimalDb extends AbstractSeed
         $this->execute('DELETE FROM `borrow_items`');
         $this->execute('DELETE FROM `borrow_locations`');
         $this->execute('DELETE FROM `cms_users`');
-        $this->execute('DELETE FROM `camps`');
         $this->execute('DELETE FROM `cms_functions`');
         $this->execute('DELETE FROM `cms_functions_camps`');
         $this->execute('DELETE FROM `cms_settings`');
@@ -24,26 +23,28 @@ class ClearMinimalDb extends AbstractSeed
         $this->execute('DELETE FROM `cms_usergroups_camps`');
         $this->execute('DELETE FROM `cms_usergroups_functions`');
         $this->execute('DELETE FROM `cms_usergroups_levels`');
-        $this->execute('DELETE FROM `genders`');
         $this->execute('DELETE FROM `history`');
         $this->execute('DELETE FROM `languages`');
         $this->execute('DELETE FROM `laundry_appointments`');
         $this->execute('DELETE FROM `laundry_machines`');
         $this->execute('DELETE FROM `laundry_slots`');
-        $this->execute('DELETE FROM `laundry_stations`');
         $this->execute('DELETE FROM `laundry_times`');
+        $this->execute('DELETE FROM `laundry_stations`');
         $this->execute('DELETE FROM `locations`');
         $this->execute('DELETE FROM `log`');
         $this->execute('DELETE FROM `need_periods`');
         $this->execute('DELETE FROM `numbers`');
-        $this->execute('DELETE FROM `organisations`');
         $this->execute('DELETE FROM `people`');
+        $this->execute('DELETE FROM `products`');
         $this->execute('DELETE FROM `product_categories`');
+        $this->execute('DELETE FROM `camps`');
+        $this->execute('DELETE FROM `organisations`');
         $this->execute('DELETE FROM `qr`');
         $this->execute('DELETE FROM `sizegroup`');
         $this->execute('DELETE FROM `tipofday`');
         $this->execute('DELETE FROM `transactions`');
         $this->execute('DELETE FROM `translate`');
+        $this->execute('DELETE FROM `genders`');
         $this->execute('DELETE FROM `units`');
     }
 }
