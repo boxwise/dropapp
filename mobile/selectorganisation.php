7836ebacb25dedca60a9672f332c391e8f0d0688
HaGuesto <hans@boxwise.co>
2019-10-28 10:46:48 +0100

    fix selectorganisation

--- mobile/selectorganisation.php
+++ mobile/selectorganisation.php
@@ -5,7 +5,7 @@ $camp = db_value('SELECT c.id AS camp_id
 	FROM qr AS q, stock AS s, locations AS l, camps AS c 
 	WHERE q.code = :barcode AND q.legacy = :legacy AND q.id = s.qr_id AND l.id = s.location_id AND c.id = l.camp_id', ['barcode' => $_GET['barcode'], 'legacy' => (isset($_GET['qrlegacy']) ? 1 : 0)]);
 if ($camp) {
-    $box = db_row('SELECT s.id location FROM stock AS s 
+    $box = db_row('SELECT s.id FROM stock AS s 
 	LEFT OUTER JOIN qr AS q ON q.id = s.qr_id
 	WHERE q.code = :barcode AND q.legacy = :legacy', ['barcode' => $_GET['barcode'], 'legacy' => (isset($_GET['qrlegacy']) ? 1 : 0)]);
     redirect('?camp='.$camp.'&boxid='.$box['id']);
