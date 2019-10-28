287352bc83af944d22a08064754968e6bc002b62
HaGuesto <hans@boxwise.co>
2019-10-28 09:51:29 +0100

    refer using box db id and not Qr-code

--- mobile/selectorganisation.php
+++ mobile/selectorganisation.php
@@ -3,9 +3,12 @@
 // if barcode is specified then redirect to the right camps
 $camp = db_value('SELECT c.id AS camp_id
 	FROM qr AS q, stock AS s, locations AS l, camps AS c 
-	WHERE q.code = :barcode AND q.id = s.qr_id AND l.id = s.location_id AND c.id = l.camp_id', ['barcode' => $_GET['barcode']]);
+	WHERE q.code = :barcode AND q.legacy = :legacy AND q.id = s.qr_id AND l.id = s.location_id AND c.id = l.camp_id', ['barcode' => $_GET['barcode'], 'legacy' => (isset($_GET['qrlegacy']) ? 1 : 0)]);
 if ($camp) {
-    redirect('?camp='.$camp.'&barcode='.$_GET['barcode']);
+    $box = db_row('SELECT s.id location FROM stock AS s 
+	LEFT OUTER JOIN qr AS q ON q.id = s.qr_id
+	WHERE q.code = :barcode AND q.legacy = :legacy', ['barcode' => $_GET['barcode'], 'legacy' => (isset($_GET['qrlegacy']) ? 1 : 0)]);
+    redirect('?camp='.$camp.'&boxid='.$box['id']);
 }
 
 $organisations = db_array('SELECT id, label FROM organisations ');
