b761e09a8edf8303e43f95edf40413a7b8207f51
HaGuesto <hans@boxwise.co>
2019-10-29 21:34:57 +0100

    Forward legacy url to app.boxwise.co with get parameter (#241)
    * Add legacy column to qr table
    
    * add permanent redirect from market.drapenihavet.no
    
    * Fix get parameter in redirection
    
    * query boxes including barcode and legacy on mobile
    
    * remove obsolete query of qr-codes
    
    * refer using box db id and not Qr-code
    
    * changes in old feature which does not exist anymor
    
    * Show qr-code with legacy parameter in stock_edit
    
    * fix selectorganisation
    
    * Reassign Qr-code to box once a new label is printe
    
    * Remove redirect from market.drapenihavet.no

--- mobile/newbox.php
+++ mobile/newbox.php
@@ -1,7 +1,9 @@
 <?php
 
-    $box['qr_id'] = db_value('SELECT id FROM qr WHERE code = :barcode', ['barcode' => $_GET['newbox']]);
-
+    $box['qr_id'] = db_value('SELECT id FROM qr WHERE code = :barcode AND legacy = :legacy', ['barcode' => $_GET['newbox'], 'legacy' => (isset($_GET['qrlegacy']) ? 1 : 0)]);
+    if (!isset($box['qr_id'])) {
+        throw new Exception('No QR-code assigned for new box!');
+    }
     $data['products'] = db_array('SELECT p.id AS value, CONCAT(p.name, " " ,IFNULL(g.label,"")) AS label, sizegroup_id FROM products AS p LEFT OUTER JOIN genders AS g ON p.gender_id = g.id WHERE (NOT p.deleted OR p.deleted IS NULL) AND camp_id = :camp_id ORDER BY name', ['camp_id' => $_SESSION['camp']['id']]);
     $data['locations'] = db_array('SELECT *, id AS value FROM locations WHERE camp_id = :camp_id ORDER BY seq', ['camp_id' => $_SESSION['camp']['id']]);
 
