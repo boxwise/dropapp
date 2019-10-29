224f78039627c9bc47ab38b80949cb5388feef66
HaGuesto <hans@boxwise.co>
2019-10-29 21:34:57 +0100

    Forward legacy url to app.boxwise.co with get parameter (#241)
    * Add legacy column to qr table
    
    * add permanent redirect from market.drapenihavet.no
    
    * Fix get parameter in redirection
    
    * query boxes including barcode and legacy on mobile
    
    * remove obsolete query of qr-codes
    
    * refer using box db id and not Qr-code
    
    * changes in old feature which does not exist anymor
    
    * Show qr-code with legacy parameter in stock_edit
    
    * fix selectorganisation
    
    * Reassign Qr-code to box once a new label is printe
    
    * Remove redirect from market.drapenihavet.no

--- mobile/barcode.php
+++ mobile/barcode.php
@@ -2,10 +2,14 @@
 
     $data['barcode'] = $_GET['barcode'];
 
-    if ($_GET['barcode'] && !db_value('SELECT id FROM qr WHERE code = :code', ['code' => $_GET['barcode']])) {
+    if ($_GET['barcode'] && !db_value('SELECT id FROM qr WHERE code = :code AND legacy = :legacy', ['code' => $_GET['barcode'], 'legacy' => (isset($_GET['qrlegacy']) ? 1 : 0)])) {
         $data['warning'] = true;
         $data['message'] = 'This is not a valid QR-code for '.$_SESSION['organisation']['label'];
         $data['barcode'] = '';
+        // Check if it is a legacy error
+        if (db_value('SELECT id FROM qr WHERE code = :code AND legacy = :legacy', ['code' => $_GET['barcode'], 'legacy' => (!isset($_GET['qrlegacy']) ? 1 : 0)])) {
+            Sentry\captureMessage('Scanned QR-code exist in qr-table, but with different legacy value!');
+        }
     } else {
         if ($_GET['boxid']) {
             $box = db_row('SELECT s.*, c.id AS camp_id, c.name AS campname, CONCAT(p.name," ",g.label," ",IFNULL(s2.label, "")) AS product, p.name AS product2, g.label AS gender, IFNULL(s2.label, "") AS size, l.label AS location FROM stock AS s
@@ -15,8 +19,9 @@
 		LEFT OUTER JOIN locations AS l ON l.id = s.location_id
 		LEFT OUTER JOIN qr AS q ON q.id = s.qr_id
 		LEFT OUTER JOIN camps AS c ON c.id = l.camp_id
-		WHERE (NOT s.deleted OR s.deleted IS NULL) AND s.id = :id', ['id' => $_GET['boxid']]);
+		WHERE s.id = :id', ['id' => $_GET['boxid']]);
         } else {
+            $qr_id = db_value('SELECT id FROM qr WHERE code = :code AND legacy = :legacy', ['code' => $_GET['barcode'], 'legacy' => (isset($_GET['qrlegacy']) ? 1 : 0)]);
             $box = db_row('SELECT s.*, c.id AS camp_id, c.name AS campname, CONCAT(p.name," ",g.label," ",IFNULL(s2.label, "")) AS product, p.name AS product2, g.label AS gender, IFNULL(s2.label, "") AS size, l.label AS location FROM stock AS s
 		LEFT OUTER JOIN products AS p ON p.id = s.product_id
 		LEFT OUTER JOIN genders AS g ON g.id = p.gender_id
@@ -24,7 +29,7 @@
 		LEFT OUTER JOIN locations AS l ON l.id = s.location_id
 		LEFT OUTER JOIN qr AS q ON q.id = s.qr_id
 		LEFT OUTER JOIN camps AS c ON c.id = l.camp_id
-		WHERE q.code = :barcode', ['barcode' => $data['barcode']]);
+		WHERE q.id = :qrid', ['qrid' => $qr_id]);
         }
         if ('0000-00-00 00:00:00' != $box['deleted'] && !is_null($box['deleted'])) {
             $tpl->assign('include', 'mobile_editbox.tpl');
@@ -44,7 +49,7 @@
                 $tpl->assign('locations', $locations);
                 $tpl->assign('include', 'mobile_scan.tpl');
             } else {
-                redirect('?newbox='.$data['barcode']);
+                redirect('?newbox='.$data['barcode'].(isset($_GET['qrlegacy']) ? '&qrlegacy=1' : ''));
             }
         }
     }
