444d00f67e481b8ba5e2608165c834deaea6abbf
HaGuesto <hans@boxwise.co>
2019-11-14 20:51:00 +0100

    Format smarty

--- library/lib/smarty.php
+++ library/lib/smarty.php
@@ -16,7 +16,7 @@ class Zmarty extends Smarty
         if (isset($_GET['smartydebug'])) {
             $this->debugging = true;
         }
-        
+
         $this->escape_html = true;
         $this->merge_compiled_includes = true;
         $this->setCompileDir(__DIR__.'/../../templates/templates_c');
