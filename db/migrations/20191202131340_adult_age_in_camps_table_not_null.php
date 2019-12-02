ed80c575321cdb43d33ac3b47c535f9ca970cad5
HaGuesto <hans@boxwise.co>
2019-12-02 14:20:53 +0100

    make adult-age in camp table not nullable

---
+++ db/migrations/20191202131340_adult_age_in_camps_table_not_null.php
@@ -0,0 +1,15 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AdultAgeInCampsTableNotNull extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('camps')
+        ->changeColumn('adult_age', 'integer', [
+            'null' => false,
+            'default' => 15,
+        ])->save();
+    }
+}
