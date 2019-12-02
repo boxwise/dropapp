c39d66fb54a3a5a53ebcb1f572a3d77e3d985a1b
HaGuesto <hans@boxwise.co>
2019-12-02 15:01:18 +0100

    make user_id in transactions db nullable

---
+++ db/migrations/20191202135024_user_id_in_transactions_nullable.php
@@ -0,0 +1,14 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class UserIdInTransactionsNullable extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('transactions')
+        ->changeColumn('user_id', 'integer', [
+            'null' => true,
+        ])->save();
+    }
+}
