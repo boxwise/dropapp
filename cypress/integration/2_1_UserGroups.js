a680564bfa97a8bedd4f7634310f424e1fbd2a09
Martin Ďurček <nitramdurcek@gmail.com>
2019-12-18 09:51:28 +0100

    2.1 - 2.3 Usergroup browser tests (#288)
    * delete: duplicate select2 interaction cypress file
    
    * add: usergroup tests (admin)
    
    * add: usergroup browser tests for coordinator
    
    * change: minor changes in usergroup browser tests
    
    * change: minor usergroup tests visual changes
    
    * add test: check visible available bases in usergroup form
    
    * add: add select2 helper function to check the count of options
    
    * replace word delete with remove in test 2_1
    
    * change: correct selector for camps dropdown in usergroup tests

---
+++ cypress/integration/2_1_UserGroups.js
@@ -0,0 +1,270 @@
+//test usergroup data to be created
+const BrowserTestUserGroup_Coordinator = "BrowserTestUserGroup_Coordinator";
+const BrowserTestUserGroup_User = "BrowserTestUserGroup_User";
+const TestBase = "TestBase";
+const TestBaseOfAnotherOrg = "DummyTestBaseWithBoxes"
+const TestUserGroupFunctions = ["Boxes", "Checkout"];
+
+// data from seed
+const AllFunctions = ['Free Shop', 'Admin','Users','Settings','Texts','Manage menu functions','Manage products',
+'Checkout','Boxes','Find beneficiary','Give tokens to all','Sales reports','Inventory','Fancy graphs',
+'Stockroom','Generate market schedule','Generate QR labels','Actions','Warehouses','Containers List',
+'Needed items','Manage beneficiaries','Hidden menu items','Start page','Give tokens to selected families',
+'User profile','Exit login as','Sales List Download','Insight','General stock','Lists','Services',
+'Bicycles / Sport','Borrow edit','Borrow history ','Library','Borrow books','Library history',
+'Library','Laundry','All Residents export','Laundry No show','Laundry start new cycle','Organisations',
+'Boxwise Gods','User groups','Bases','Add beneficiary'];
+const AdminAvailableFunctions = ['Users','Manage products',
+'Checkout','Boxes','Give tokens to all','Fancy graphs', 'Find beneficiary',
+'Stockroom','Generate market schedule','Generate QR labels',
+'Needed items','Manage beneficiaries', 'Sales reports',
+'User groups','Add beneficiary'];
+const CoordinatorAvailableFunctions = ['Checkout','Boxes','Fancy graphs', 'Find beneficiary',
+'Stockroom','Generate QR labels','Users','Manage products','Generate market schedule',
+'Needed items','Manage beneficiaries', 'Sales reports','Give tokens to all',
+'User groups','Add beneficiary'];
+
+const AllLevels = ["Coordinator", "User", "Admin"]
+const LevelsVisibleToAdmin = ["Coordinator", "User"];
+const LevelsVisibleToCoordinator = ["User"];
+
+const TestUserGroupWithUsersAssigned = "TestUserGroup_NoPermissions";
+const TestUserGroup_Coordinator = "TestUserGroup_Coordinator";
+const TestUserGroup_User = "TestUserGroup_User"
+
+
+function userGroupFormElementsAreVisible(){
+    cy.get("input[data-testid='userGroupName']").should('be.visible');
+    cy.get("select[data-testid='userGroupLevel']").should('be.visible');
+    cy.get("select[data-testid='userGroupBases']").should('be.visible');
+    cy.get("select[data-testid='userGroupFunctions']").should('be.visible');
+}
+
+function getOption(lvl){
+    return cy.get("ul[class='select2-results'] li").contains(lvl);
+}
+
+function clickButtonWithText(buttontext) {
+    cy.get("button").contains(buttontext).click();
+}
+
+function checkUserGroupCheckboxByName(name){
+    getUserGroupRow(name).parent().parent().parent().within(() => {
+        cy.get("input[type='checkbox']").check();
+    });
+}
+
+function getUserGroupRow(name){
+    return cy.get('tr').contains(name);
+}
+
+function deleteUserGroup(name) {
+    cy.get('body').then(($body) => {
+        if ($body.text().includes(name)) {
+            checkUserGroupCheckboxByName(name)
+            clickDeleteButton();
+            confirmAction();
+        }
+    });
+}
+
+function FillUserGroupForm(name, level, availableBase, availableFunctions){
+    cy.get("input[data-testid='userGroupName']").clear().type(name);
+    cy.selectOptionByText('userlevel', level);
+    cy.selectOptionByText('camps', availableBase);
+    for (var fcn of availableFunctions) {
+        cy.selectOptionByText("cms_functions",fcn);
+    }
+}
+
+function clickDeleteButton(){
+    cy.get("button[data-testid='list-delete-button']").click();
+}
+
+function confirmAction(){
+    cy.get("a[data-apply='confirmation']").click();
+}
+
+function checkQtip(qtipText) {
+    cy.get("div[class='qtip-content']").contains(qtipText).should("be.visible");
+}
+
+describe('Create usergroups (admin)', () => {
+    beforeEach(function () {
+        cy.loginAsAdmin();
+        cy.visit('/?action=cms_usergroups_edit&origin=cms_usergroups');
+    });
+
+    it("Create & delete 'coordinator' usergroup", () => {
+        FillUserGroupForm(BrowserTestUserGroup_Coordinator, "Coordinator", TestBase, TestUserGroupFunctions)
+        clickButtonWithText("Save and close");
+        getUserGroupRow(BrowserTestUserGroup_Coordinator).should('exist');
+        //testing delete
+        checkUserGroupCheckboxByName(BrowserTestUserGroup_Coordinator)
+        clickDeleteButton();
+        confirmAction();
+        getUserGroupRow(BrowserTestUserGroup_Coordinator).should('not.exist');
+        cy.notyTextNotificationWithTextIsVisible("Item deleted");
+
+        //cleanup just in case
+        deleteUserGroup(BrowserTestUserGroup_Coordinator);
+    });
+
+    it("Create & delete 'user' usergroup", () => {
+        FillUserGroupForm(BrowserTestUserGroup_User, "User", TestBase, TestUserGroupFunctions)
+        clickButtonWithText("Save and close");
+        getUserGroupRow(BrowserTestUserGroup_User).should('exist');
+        //testing delete
+        checkUserGroupCheckboxByName(BrowserTestUserGroup_User)
+        clickDeleteButton();
+        confirmAction();
+        getUserGroupRow(BrowserTestUserGroup_User).should('not.exist');
+        cy.notyTextNotificationWithTextIsVisible("Item deleted");
+
+        //cleanup just in case
+        deleteUserGroup(BrowserTestUserGroup_User);
+    });
+
+    it('Prevent usergroup creation without base', () => {
+        cy.get("input[data-testid='userGroupName']").clear().type(BrowserTestUserGroup_Coordinator);
+        cy.selectOptionByText('userlevel', "Coordinator");
+        for (var fcn of TestUserGroupFunctions) {
+            cy.selectOptionByText("cms_functions",fcn);
+        }
+        clickButtonWithText("Save and close");
+        checkQtip("This field is required");
+        userGroupFormElementsAreVisible();
+    });
+
+    it('Prevent usergroup creation without functions', () => {
+        cy.get("input[data-testid='userGroupName']").clear().type(BrowserTestUserGroup_Coordinator);
+        cy.selectOptionByText('userlevel', "Coordinator");
+        cy.selectOptionByText('camps', TestBase);
+        clickButtonWithText("Save and close");
+        checkQtip("This field is required");
+        userGroupFormElementsAreVisible();
+    });
+
+    it("Check available usergroup levels", () => {
+        cy.clickSelect("userlevel");
+        for (var lvl of AllLevels){
+            if (LevelsVisibleToAdmin.includes(lvl)) {
+                getOption(lvl).should('exist');
+            } else {
+                getOption(lvl).should('not.exist');
+            }
+            
+        }
+    });
+
+    it('Check available bases', () => {
+        cy.clickSelect("camps");
+        getOption(TestBase).should('exist');
+        getOption(TestBaseOfAnotherOrg).should('not.exist');
+        cy.checkOptionsCount("camps", 1);
+    });
+
+    it("Check available functions", () => {
+        cy.clickSelect("cms_functions");
+        for (var fcn of AllFunctions){
+            if (AdminAvailableFunctions.includes(fcn)) {
+                getOption(fcn).should('exist');
+            } else {
+                getOption(fcn).should('not.exist');
+            }
+            
+        }
+    });
+
+    it('Prevent deletion of usergroup with users', () => {
+        cy.visit('/?action=cms_usergroups');
+        checkUserGroupCheckboxByName(TestUserGroupWithUsersAssigned)
+        clickDeleteButton();
+        confirmAction();
+        cy.notyTextNotificationWithTextIsVisible("Please edit or remove it first");
+        getUserGroupRow(TestUserGroupWithUsersAssigned).should('exist');
+    });
+});
+
+
+describe('Create usergroups (coordinator)', () => {
+    beforeEach(function () {
+        cy.loginAsCoordinator();
+        cy.visit('/?action=cms_usergroups_edit&origin=cms_usergroups');
+    });
+
+    it("Create & delete 'user' usergroup", () => {
+        FillUserGroupForm(BrowserTestUserGroup_User, "User", TestBase, TestUserGroupFunctions)
+        clickButtonWithText("Save and close");
+        getUserGroupRow(BrowserTestUserGroup_User).should('exist');
+        //testing delete
+        checkUserGroupCheckboxByName(BrowserTestUserGroup_User)
+        clickDeleteButton();
+        confirmAction();
+        getUserGroupRow(BrowserTestUserGroup_User).should('not.exist');
+        cy.notyTextNotificationWithTextIsVisible("Item deleted");
+
+        //cleanup just in case
+        deleteUserGroup(BrowserTestUserGroup_User);
+    });
+
+    it('Prevent usergroup creation without base', () => {
+        cy.get("input[data-testid='userGroupName']").clear().type(BrowserTestUserGroup_Coordinator);
+        cy.selectOptionByText('userlevel', "User");
+        for (var fcn of TestUserGroupFunctions) {
+            cy.selectOptionByText("cms_functions",fcn);
+        }
+        clickButtonWithText("Save and close");
+        checkQtip("This field is required");
+        userGroupFormElementsAreVisible();
+    });
+
+    it('Prevent usergroup creation without functions', () => {
+        cy.get("input[data-testid='userGroupName']").clear().type(BrowserTestUserGroup_Coordinator);
+        cy.selectOptionByText('userlevel', "User");
+        cy.selectOptionByText('camps', TestBase);
+        clickButtonWithText("Save and close");
+        checkQtip("This field is required");
+        userGroupFormElementsAreVisible();
+    });
+
+    it("Check available usergroup levels", () => {
+        cy.clickSelect("userlevel");
+        for (var lvl of AllLevels){
+            if (LevelsVisibleToCoordinator.includes(lvl)) {
+                getOption(lvl).should('exist');
+            } else {
+                getOption(lvl).should('not.exist');
+            }
+            
+        }
+    });
+
+    it('Check available bases', () => {
+        cy.clickSelect("camps");
+        getOption(TestBase).should('exist');
+        getOption(TestBaseOfAnotherOrg).should('not.exist');
+        cy.checkOptionsCount("camps", 1);
+    });
+
+    it("Check available functions", () => {
+        cy.clickSelect("cms_functions");
+        for (var fcn of AllFunctions){
+            if (CoordinatorAvailableFunctions.includes(fcn)) {
+                getOption(fcn).should('exist');
+            } else {
+                getOption(fcn).should('not.exist');
+            }
+            
+        }
+    });
+
+    it('Prevent deletion of usergroup with users', () => {
+        cy.visit('/?action=cms_usergroups');
+        checkUserGroupCheckboxByName(TestUserGroupWithUsersAssigned)
+        clickDeleteButton();
+        confirmAction();
+        cy.notyTextNotificationWithTextIsVisible("Please edit or remove it first");
+        getUserGroupRow(TestUserGroupWithUsersAssigned).should('exist');
+    });
+});
--- cypress/support/select2-interaction-methods.js
+++ cypress/support/select2-interaction-methods.js
@@ -1,6 +1,10 @@
 /* Helper functions to wrap interactions with boxwise-custom select dropdown DOM element */
 /* field_id is a unique identifier in a form this dropdown is used to select values for */
 
+Cypress.Commands.add("clickSelect", (field_id) => {
+    cy.get("div[id='s2id_field_" + field_id + "']").click();
+});
+
 Cypress.Commands.add("selectOptionByText", (field_id, optionText) => {
     cy.get("div[id='s2id_field_" + field_id + "']").click();
     cy.get("ul[class='select2-results'] li").contains(optionText).click();
@@ -16,3 +20,11 @@ Cypress.Commands.add("selectForFieldExists", (field_id) => {
     cy.get("div[id='s2id_field_" + field_id + "']").should('exist');
 });
 
+Cypress.Commands.add("checkOptionsCount", (field_id, count) => {
+    cy.get("div[id='s2id_field_" + field_id + "']").next().within(()=>{
+        cy.get("option").then($options => {
+            //there's one empty option when nothing is selected
+            expect($options.length).to.equal(count+1);
+        })
+    })
+});
