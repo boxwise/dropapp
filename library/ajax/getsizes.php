83db8f7a1629cbd60e4c25b28303070dab13c0c1
lea cohen <cohenswim08@gmail.com>
2019-12-30 22:05:40 +0800

    selected class if only one size

--- library/ajax/getsizes.php
+++ library/ajax/getsizes.php
@@ -3,13 +3,17 @@
     $success = true;
     $product_id = $_POST['product_id'];
     $size_id = intval($_GET['size']);
-    $one_size_id = 68;
 
-    $result = db_query('SELECT s.* FROM sizes AS s, sizegroup AS sg, products AS p WHERE s.sizegroup_id = sg.id AND p.id = :id AND p.sizegroup_id = sg.id ORDER BY s.seq', ['id' => $product_id]);
+    $result = db_array('SELECT s.* FROM sizes AS s, sizegroup AS sg, products AS p WHERE s.sizegroup_id = sg.id AND p.id = :id AND p.sizegroup_id = sg.id ORDER BY s.seq', ['id' => $product_id]);
+
     $html = '<option></option>';
 
-    while ($row = db_fetch($result)) {
-        $html .= '<option value="'.$row['id'].'" '.(($size_id == $row['id'] || $row['id'] == $one_size_id) ? 'selected' : '').'>'.$row['label'].'</option>';
+    if (count($result) == 1) {
+        $html .= '<option value="'.$result[0]['id'].'" selected>'.$result[0]['label'].'</option>';
+    } else {
+        foreach ($result as $row) {
+            $html .= '<option value="'.$row['id'].'" '.($size_id == $row['id'] ? 'selected' : '').'>'.$row['label'].'</option>';
+        }
     }
 
     $return = ['success' => $success, 'html' => $html, 'message' => $message];
