378024d938d09780602b2d878c2b8aad3fac9f62
Lea Cohen <cohenswim08@gmail.com>
2020-01-03 21:58:47 +0700

    add selected attribute to form option if only one option for field (#294)
    * add .selected class to size option if the product is one size only
    
    * selected class if only one size
    
    * trying again
    
    * autoselect option if only one option and it's not product
    
    * fixed format issue

--- library/ajax/getsizes.php
+++ library/ajax/getsizes.php
@@ -4,10 +4,15 @@
     $product_id = $_POST['product_id'];
     $size_id = intval($_GET['size']);
 
-    $result = db_query('SELECT s.* FROM sizes AS s, sizegroup AS sg, products AS p WHERE s.sizegroup_id = sg.id AND p.id = :id AND p.sizegroup_id = sg.id ORDER BY s.seq', ['id' => $product_id]);
+    $result = db_array('SELECT s.* FROM sizes AS s, sizegroup AS sg, products AS p WHERE s.sizegroup_id = sg.id AND p.id = :id AND p.sizegroup_id = sg.id ORDER BY s.seq', ['id' => $product_id]);
     $html = '<option></option>';
-    while ($row = db_fetch($result)) {
-        $html .= '<option value="'.$row['id'].'" '.($size_id == $row['id'] ? 'selected' : '').'>'.$row['label'].'</option>';
+
+    if (1 == count($result)) {
+        $html .= '<option value="'.$result[0]['id'].'" selected>'.$result[0]['label'].'</option>';
+    } else {
+        foreach ($result as $row) {
+            $html .= '<option value="'.$row['id'].'" '.($size_id == $row['id'] ? 'selected' : '').'>'.$row['label'].'</option>';
+        }
     }
 
     $return = ['success' => $success, 'html' => $html, 'message' => $message];
