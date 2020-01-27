eb6b959a39426b86374c6fd42144b73af7081b03
stephan <naphets123@gmail.com>
2020-01-27 17:00:34 +0100

    Added coverage for case of success variable not set

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -162,7 +162,12 @@ function AjaxCheckSuccess(result){
         if (result.action) {
             eval(result.action);
             }
-
+        if (result.success == 'undefined') {
+            var n = noty({
+                text: "Something went wrong - please inform your coordinator.",
+                type: "error"
+                });
+        }
 
 
 }
