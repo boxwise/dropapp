4ec614d4632ad903eb85195c0b69bc1c3d589014
lea cohen <cohenswim08@gmail.com>
2020-02-03 02:07:49 +0530

    in progress

--- include/locations_edit.php
+++ include/locations_edit.php
@@ -2,11 +2,7 @@
 
     $table = 'locations';
     $action = 'locations_edit';
-    $is_admin = false;
-
-    if ($_SESSION['user']['is_admin']) {
-        $is_admin = true;
-    }
+    $is_admin = ($_SESSION['user']['is_admin']);
 
     if ($_POST) {
         verify_campaccess_location($_POST['id']);
@@ -14,6 +10,7 @@
             throw new Exception("You don't have access to this record");
         }
 
+        $_POST['visible'] = !$_POST['outgoing'];
         $handler = new formHandler($table);
 
         $savekeys = ['label', 'visible', 'camp_id', 'container_stock', 'is_market', 'is_donated', 'is_lost'];
@@ -22,14 +19,17 @@
         redirect('?action='.$_POST['_origin']);
     }
 
-    $data = db_row('SELECT * FROM '.$table.' WHERE id = :id', ['id' => $id]);
+    $data = db_row('SELECT *, NOT visible as outgoing FROM '.$table.' WHERE id = :id', ['id' => $id]);
 
     //verify_campaccess_location($id);
 
     if (!$id) {
-        $data['visible'] = 0;
+        $data = db_defaults($table);
         $data['container_stock'] = 1;
         $data['camp_id'] = $_SESSION['camp']['id'];
+        if ($is_admin) {
+            $data['visible'] = 0;
+        }
     }
 
     // open the template
@@ -43,10 +43,12 @@
     addfield('text', 'Label', 'label');
     if ($is_admin) {
         addfield('checkbox', 'Stockroom', 'container_stock');
-        addfield('checkbox', 'Market', 'is_market');
+        addfield('checkbox', 'Shop', 'is_market');
         addfield('checkbox', 'Donated', 'is_donated');
         addfield('checkbox', 'Lost', 'is_lost');
-        addfield('checkbox', 'This warehouse location is an outgoing location', 'visible', ['aside' => true, 'tooltip' => 'Items in outgoing warehouse locations are not counted as part of your stock.']);
+        addfield('checkbox', 'This warehouse location is an outgoing location', 'outgoing', ['tooltip' => 'Items in outgoing warehouse locations are not counted as part of your stock.']);
+    } else {
+        addfield('hidden', '', 'visible');
     }
     addfield('line', '', '', ['aside' => true]);
     addfield('created', 'Created', 'created', ['aside' => true]);
