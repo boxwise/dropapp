941c60eb4214d373c5c4703fb0c2e1ac776156b1
naphets123 <42490840+naphets123@users.noreply.github.com>
2020-06-08 19:22:01 +0200

    Fixed seed (#357)
    * Fixed names, email and id for demodb seeds
    
    * Fixed editing
    
    * Update DemoDb.php
    
    * Fix format
    
    Co-authored-by: HaGuesto <hans@boxwise.co>

--- library/lib/auth0.php
+++ library/lib/auth0.php
@@ -4,10 +4,10 @@ require 'vendor/autoload.php';
 use Auth0\SDK\Auth0;
 
 $auth0 = new Auth0([
-  'domain' => $settings['auth0_domain'],
-  'client_id' => $settings['auth0_client_id'],
-  'client_secret' => $settings['auth0_client_secret'],
-  'redirect_uri' => $settings['auth0_redirect_uri'],
+    'domain' => $settings['auth0_domain'],
+    'client_id' => $settings['auth0_client_id'],
+    'client_secret' => $settings['auth0_client_secret'],
+    'redirect_uri' => $settings['auth0_redirect_uri'],
 ]);
 
 $userInfo = $auth0->getUser();
