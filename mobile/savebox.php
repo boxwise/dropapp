7941a6380b56d94d2e997369b8a7de9e50b4f6ad
HaGuesto <hans@boxwise.co>
2020-12-01 21:55:31 +0100

    Validate whether a Box is new or existing mobile

--- mobile/savebox.php
+++ mobile/savebox.php
@@ -1,76 +1,75 @@
 <?php
 
-if (!isset($_POST)) {
+use OpenCensus\Trace\Tracer;
+
+// Validation
+if (!$_POST) {
     trigger_error('No POST data was passed to mobile savebox script.', E_USER_ERROR);
     redirect('?warning=1&message=Something went wrong! Please try again!');
-
-    return;
+}
+$box = db_row('SELECT * FROM stock WHERE id = :id', ['id' => $_POST['id']]);
+$new = !$box;
+// Validate that an qr_id was submitted
+if (!$_POST['qr_id']) {
+    trigger_error('No QR-code associated to '.($new ? 'new' : 'existing').'box.', E_USER_ERROR);
+    redirect('?warning=1&message=Something went wrong! Please try again!');
+} else {
+    // Check if other Box was already created with this QR-code
+    if ($new && db_value('SELECT id FROM stock WHERE qr_id = :qrid', ['qrid' => $_POST['qr_id']])) {
+        $message = 'You have already created a Box with this QR-code!';
+        trigger_error($message, E_USER_ERROR);
+        redirect('?warning=1&message='.$message);
+    }
 }
 
-    if (!intval($_POST['box_id'])) {
-        $new = true;
-        do {
-            $_POST['box_id'] = generateBoxID();
-        } while (db_value('SELECT COUNT(id) FROM stock WHERE box_id = :box_id', ['box_id' => $_POST['box_id']]));
-    }
-
-    // Load box
-    $box = db_row('SELECT * FROM stock WHERE id = :id', ['id' => $_POST['id']]);
-
+// Updates and Preparation
+if ($new) {
+    Tracer::inSpan(
+        ['name' => ('mobile/savebox.php:Box ID Generation')],
+        function () {
+            do {
+                $_POST['box_id'] = generateBoxID();
+            } while (db_value('SELECT COUNT(id) FROM stock WHERE box_id = :box_id', ['box_id' => $_POST['box_id']]));
+        });
+} else {
     // unset order state
-    if ($box && $box['location_id'] != $_POST['location_id'][0]) {
+    if ($box['location_id'] != $_POST['location_id']) {
         db_query('UPDATE stock SET ordered = NULL, ordered_by = NULL, picked = NULL, picked_by = NULL WHERE id = :id', ['id' => $box['id']]);
-        db_query('INSERT INTO itemsout (product_id, size_id, count, movedate, from_location, to_location) VALUES ('.$box['product_id'].','.$box['size_id'].','.$box['items'].',NOW(),'.$box['location_id'].','.$_POST['location_id'][0].')');
+        db_query('INSERT INTO itemsout (product_id, size_id, count, movedate, from_location, to_location) VALUES ('.$box['product_id'].','.$box['size_id'].','.$box['items'].',NOW(),'.$box['location_id'].','.$_POST['location_id'].')');
     }
 
     // Undelete a box if it is scanned
-    if ($box && ('0000-00-00 00:00:00' != $box['deleted'] && !is_null($box['deleted']))) {
+    if ('0000-00-00 00:00:00' != $box['deleted'] && !is_null($box['deleted'])) {
         db_query('UPDATE stock SET deleted = "0000-00-00 00:00" WHERE id = :id', ['id' => $_POST['id']]);
     }
+}
 
-    // keys of POST to be saved
-    $savekeys = ['box_id', 'product_id', 'size_id', 'items', 'location_id', 'comments'];
+// keys of POST to be saved
+$savekeys = ['box_id', 'product_id', 'size_id', 'items', 'location_id', 'comments', 'qr_id'];
+if (!$new) {
+    $savekeys[] = 'id';
+}
 
-    // Validate QR-code
-    if (!$_POST['qr_id']) {
-        if ($new) {
-            trigger_error('No QR-code associated to new box.', E_USER_ERROR);
-            redirect('?warning=1&message=Something went wrong! Please try again!');
-        } else {
-            trigger_error('No QR-code associated to existing box.');
-        }
-    } else {
-        // add qr_id key only to formhandler if POST entry exists
-        $savekeys[] = 'qr_id';
-    }
+$handler = new formHandler('stock');
 
-    $handler = new formHandler('stock');
+$id = $handler->savePost($savekeys);
 
-    if ($_POST['id']) {
-        $savekeys[] = 'id';
-    }
-    $id = $handler->savePost($savekeys);
+// Log qr box connection in history table
+if ($new) {
+    simpleSaveChangeHistory('qr', $_POST['qr_id'], 'QR code associated to box.', [], ['int' => $id]);
+}
 
-    // Log qr box connection in history table
-    if ($new) {
-        simpleSaveChangeHistory('qr', $_POST['qr_id'], 'QR code associated to box.', [], ['int' => $id]);
-    }
+$box = db_row('SELECT s.*, CONCAT(p.name," ",g.label) AS product, l.label AS location FROM stock AS s LEFT OUTER JOIN products AS p ON p.id = s.product_id LEFT OUTER JOIN genders AS g ON g.id = p.gender_id LEFT OUTER JOIN locations AS l ON l.id = s.location_id WHERE s.id = :id', ['id' => $id]);
 
-    $box = db_row('SELECT s.*, CONCAT(p.name," ",g.label) AS product, l.label AS location FROM stock AS s LEFT OUTER JOIN products AS p ON p.id = s.product_id LEFT OUTER JOIN genders AS g ON g.id = p.gender_id LEFT OUTER JOIN locations AS l ON l.id = s.location_id WHERE s.id = :id', ['id' => $id]);
+// Validate QR-code again
+if (!$box['qr_id']) {
+    trigger_error('No QR-code associated to '.($new ? 'new' : 'existing').' box after fromhandler.', E_USER_ERROR);
+}
 
-    // Validate QR-code again
-    if (!$box['qr_id']) {
-        if ($new) {
-            trigger_error('No QR-code associated to new box after fromhandler.', E_USER_ERROR);
-        } else {
-            trigger_error('No QR-code associated to existing box after fromhandler.');
-        }
-    }
+if (!$new) {
+    $message = 'Box '.$box['box_id'].' modified with '.$box['product'].' ('.$box['items'].'x) in '.$box['location'].'. <a href="?boxid='.$box['id'].'">Go back to this box.</a>';
+} else {
+    $message = 'New box with box ID <strong class="bigger">'.$box['box_id'].'</strong> (write this number on the box label). This box contains '.$box['items'].' '.$box['product'].' and is located in '.$box['location'].'. <a href="?boxid='.$box['id'].'">Edit this box.</a>';
+}
 
-    if (!$new) {
-        $message = 'Box '.$box['box_id'].' modified with '.$box['product'].' ('.$box['items'].'x) in '.$box['location'].'. <a href="?boxid='.$box['id'].'">Go back to this box.</a>';
-    } else {
-        $message = 'New box with box ID <strong class="bigger">'.$box['box_id'].'</strong> (write this number on the box label). This box contains '.$box['items'].' '.$box['product'].' and is located in '.$box['location'].'. <a href="?boxid='.$box['id'].'">Edit this box.</a>';
-    }
-
-    redirect('?boxid='.$box['id'].'&message='.$message);
+redirect('?boxid='.$box['id'].'&message='.$message);
