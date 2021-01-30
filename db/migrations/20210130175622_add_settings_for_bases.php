3c46c0f6404bd0757666dad04716960e267387bb
HaGuesto <hans@boxwise.co>
2021-01-30 21:07:46 +0100

    Fix format

--- db/migrations/20210130175622_add_settings_for_bases.php
+++ db/migrations/20210130175622_add_settings_for_bases.php
@@ -7,10 +7,10 @@ class AddSettingsForBases extends AbstractMigration
     public function change()
     {
         $this->table('camps')
-        ->addColumn('beneficiaryisregistered', 'boolean', ['default' => 1, 'null' => false])
-        ->addColumn('beneficiaryisvolunteer', 'boolean', ['default' => 1, 'null' => false])
-        ->addColumn('separateshopandwhproducts', 'boolean', ['default' => 0, 'null' => false])
-        ->save()
+            ->addColumn('beneficiaryisregistered', 'boolean', ['default' => 1, 'null' => false])
+            ->addColumn('beneficiaryisvolunteer', 'boolean', ['default' => 1, 'null' => false])
+            ->addColumn('separateshopandwhproducts', 'boolean', ['default' => 0, 'null' => false])
+            ->save()
     ;
     }
 }
