5c5d2cdbce8eb5689cf2ed9c6e1c831bc5b924ec
Vahid Bazzaz <vahidh@gmail.com>
2021-08-06 21:24:45 +0430

    fix redirect to auth0 when user deactivate itself

--- library/ajax/deleteprofile.php
+++ library/ajax/deleteprofile.php
@@ -6,6 +6,9 @@ db_transaction(function () {
 });
 
 simpleSaveChangeHistory('cms_users', $_POST['cms_user_id'], 'Record deleted without undelete');
-$return = ['success' => true, 'message' => 'You successfully deactivated your account!', 'redirect' => '/login.php'];
+// when a user deactive its account we need to ensure that user logged out immediately and then redirected to Auth0 login page
+global $settings;
+logout();
+$return = ['success' => true, 'message' => 'You successfully deactivated your account!', 'redirect' => 'https://'.$settings['auth0_domain'].'/v2/logout?client_id='.$settings['auth0_client_id'].'&returnTo='.urlencode($settings['auth0_redirect_uri'])];
 
 echo json_encode($return);
