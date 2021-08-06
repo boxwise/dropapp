9b99208fb95dc961594a4f717d9d42cc224daff7
Vahid Bazzaz <vahidh@gmail.com>
2021-08-06 21:22:56 +0430

    modify error page to display better error message

--- library/lib/errorhandling.php
+++ library/lib/errorhandling.php
@@ -38,14 +38,101 @@ function bootstrap_exception_handler(Throwable $ex)
     Sentry\configureScope('boxwise_sentry_scope');
     Sentry\captureException($ex);
     $eventId = Sentry\State\Hub::getCurrent()->getLastEventId();
+    // list of standard http errors
+    $http_status_codes = [
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => '(Unused)',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        418 => "I'm a teapot",
+        419 => 'Authentication Timeout',
+        420 => 'Enhance Your Calm',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        424 => 'Method Failure',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        444 => 'No Response',
+        449 => 'Retry With',
+        450 => 'Blocked by Windows Parental Controls',
+        451 => 'Unavailable For Legal Reasons',
+        494 => 'Request Header Too Large',
+        495 => 'Cert Error',
+        496 => 'No Cert',
+        497 => 'HTTP to HTTPS',
+        499 => 'Client Closed Request',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        509 => 'Bandwidth Limit Exceeded',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required',
+        598 => 'Network read timeout error',
+        599 => 'Network connect timeout error',
+    ];
     // this will only work if there hasn't already been response output
-    http_response_code(500);
+    http_response_code($ex->getCode());
     $error = new Zmarty();
-    $error->assign('error', "Sorry, an unexpected error occured and has been reported. Please quote Sentry #{$eventId}.");
-    if ('localhost' == @parse_url('http://'.$_SERVER['HTTP_HOST'], PHP_URL_HOST)) {
+    if (500 === $ex->getCode() || !$http_status_codes[$ex->getCode()]) {
+        $error->assign('title', 'Sorry, something went wrong');
+        $error->assign('error', "Sorry, an unexpected error occured and has been reported. Please quote Sentry #{$eventId}.");
+    } else {
+        $error->assign('title', $ex->getCode().' - '.$http_status_codes[$ex->getCode()]);
+        $error->assign('error', "{$ex->getMessage()}");
+        $error->assign('sentry', "For additional information, please contact support and quote Sentry #{$eventId}.");
+    }
+    // if ('localhost' == @parse_url('http://'.$_SERVER['HTTP_HOST'], PHP_URL_HOST)) {
+    //     $error->assign('exception', $ex);
+    // }
+    global $settings;
+    if ('development' == $settings['app_env']) {
         $error->assign('exception', $ex);
     }
-    $error->assign('title', 'Sorry, something went wrong');
+
     $error->display('cms_error.tpl');
     die();
 }
