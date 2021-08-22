fd42da5a29cef78de56bda05779a0c9e2482417f
HaGuesto <hans@boxwise.co>
2021-08-22 09:57:05 +0200

    Make Box State ID unsigned

---
+++ db/migrations/20210822075141_make_box_state_id_unsigned.php
@@ -0,0 +1,45 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class MakeBoxStateIdUnsigned extends AbstractMigration
+{
+    public function up()
+    {
+        $this->table('stock')
+            ->dropForeignKey('box_state_id')
+            ->save()
+        ;
+
+        $this->table('locations')
+            ->dropForeignKey('box_state_id')
+            ->save()
+        ;
+
+        $this->table('box_state')->changeColumn('id', 'integer', ['identity' => true, 'signed' => false])
+            ->save()
+        ;
+
+        $this->table('stock')
+            ->changeColumn('box_state_id', 'integer', ['signed' => false, 'null' => false])
+            ->addForeignKey('box_state_id', 'box_state', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])
+            ->save()
+        ;
+
+        $this->table('locations')->addForeignKey('box_state_id', 'box_state', 'id', [
+            'delete' => 'RESTRICT', 'update' => 'CASCADE',
+        ])
+            ->changeColumn('box_state_id', 'integer', ['signed' => false, 'null' => false])
+            ->save()
+        ;
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+    }
+}
