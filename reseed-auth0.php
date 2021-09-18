21882e3fc8e68629780a99b1c1b80d9415561443
HaGuesto <hans@boxwise.co>
2021-09-19 01:16:10 +0200

    add back checks in reseed-auth0.php

--- reseed-auth0.php
+++ reseed-auth0.php
@@ -1,12 +1,12 @@
 <?php
 
-    // if (!array_key_exists('HTTP_X_APPENGINE_CRON', $_SERVER) && 'dropapp_dev' != $settings['db_database']) {
-    //     throw new Exception('Not called from AppEngine cron service');
-    // }
-    // $permittedDatabases = ['dropapp_dev', 'dropapp_demo', 'dropapp_staging'];
-    // if (!in_array($settings['db_database'], $permittedDatabases)) {
-    //     throw new Exception('Not permitting a reset of '.$settings['db_database']);
-    // }
+    if (!array_key_exists('HTTP_X_APPENGINE_CRON', $_SERVER) && 'dropapp_dev' != $settings['db_database']) {
+        throw new Exception('Not called from AppEngine cron service');
+    }
+    $permittedDatabases = ['dropapp_dev', 'dropapp_demo', 'dropapp_staging'];
+    if (!in_array($settings['db_database'], $permittedDatabases)) {
+        throw new Exception('Not permitting a reset of '.$settings['db_database']);
+    }
 
     // update Auth0
     $bypassAuthentication = true;
