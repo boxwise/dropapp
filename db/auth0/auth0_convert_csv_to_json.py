05dfd2a5b3eea0ff872c17c55e03e453c7c8e2e4
HaGuesto <hans@boxwise.co>
2021-09-19 10:39:26 +0200

    Update script to transfer data to auth0

--- db/auth0/auth0_convert_csv_to_json.py
+++ db/auth0/auth0_convert_csv_to_json.py
@@ -5,9 +5,14 @@ import re
 
 def create_entry(raw_entry,hashfunction,encoding):
     return_dict = {}
+
     app_metadata = {'is_god':raw_entry['is_admin']}
+    if not pd.isna(raw_entry['organisation_id']):
+        app_metadata['organisation_id'] = round(raw_entry['organisation_id'])
+    if not pd.isna(raw_entry['base_ids']):
+        app_metadata['base_ids']=str(raw_entry['base_ids']).split(',')
+    
     return_dict['user_id']=str(raw_entry['id'])
-    print(return_dict)
     return_dict['name']=raw_entry['naam']
     if not pd.isna(raw_entry['deleted']) and raw_entry['deleted'] != '0000-00-00 00:00:00':
         return_dict['email']=re.sub(r'\.deleted\.\d+', '',raw_entry['email'])
@@ -23,12 +28,17 @@ def create_entry(raw_entry,hashfunction,encoding):
             "encoding":encoding
         }
     }
+
     if not pd.isna(raw_entry['cms_usergroups_id']):
         app_metadata['usergroup_id']=int(round(raw_entry['cms_usergroups_id']))
     if not pd.isna(raw_entry['valid_firstday']) and raw_entry['valid_firstday'] != '0000-00-00':
         app_metadata['valid_firstday']=raw_entry['valid_firstday']
     if not pd.isna(raw_entry['valid_lastday']) and raw_entry['valid_lastday'] != '0000-00-00':
         app_metadata['valid_lastday']=raw_entry['valid_lastday']   
+    
+    print(return_dict)
+    print(app_metadata)
+
     return_dict['app_metadata'] =app_metadata
     return return_dict
 
@@ -36,7 +46,7 @@ def create_entry(raw_entry,hashfunction,encoding):
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 # load users for auth0 connection
 users = pd.read_csv('users.csv')
-usersdict = users[['id','email','pass','naam','cms_usergroups_id','is_admin','valid_firstday','valid_lastday','deleted']].T.to_dict()
+usersdict = users[['id','email','pass','naam','cms_usergroups_id','is_admin','valid_firstday','valid_lastday','deleted', 'organisation_id', 'base_ids']].T.to_dict()
 # convert to auth0 readable data
 auth0_dict = {}
 for key in usersdict:
