5a0f3d827bcf009918b3c6f32fa10a8b1b8ee85b
HaGuesto <hans@boxwise.co>
2022-01-23 14:08:56 +0100

    Fix redirect tests

--- cypress/integration/99_redirect_domain.js
+++ cypress/integration/99_redirect_domain.js
@@ -4,19 +4,32 @@ describe('Redirect domain', () => {
     let config = getLoginConfiguration();
 
     it('Navigate to staging.boxwise.co and end up at staging.boxtribute.org', () => {
-        cy.visit('https://staging.boxwise.co/?action=stock');
-        cy.fillLoginForm()
-        cy.url().should('include', 'https://staging.boxtribute.org/?action=stock');
+        cy.request({
+            url: 'https://staging.boxwise.co/?action=stock',
+            followRedirect: false, // turn off following redirects
+          }).then((resp) => {
+            expect(resp.status).to.eq(301)
+            expect(resp.redirectedToUrl).to.include('https://staging.boxtribute.org/?action=stock');
+          })
     });
 
     it('Navigate to staging.boxwise.co and end up at staging.boxtribute.org', () => {
-        cy.visit('https://staging.boxwise.co/mobile.php?barcode=test');
-        cy.fillLoginForm()
-        cy.url().should('include', 'https://staging.boxtribute.org/mobile.php?barcode=test');
+        cy.request({
+            url: 'https://staging.boxwise.co/mobile.php?barcode=test',
+            followRedirect: false, // turn off following redirects
+          }).then((resp) => {
+            expect(resp.status).to.eq(301)
+            expect(resp.redirectedToUrl).to.include('https://staging.boxtribute.org/mobile.php?barcode=test');
+          })
     });
 
     it('Navigate to market.drapenihavet.no and end up at app.boxtribute.org', () => {
-        // cy.visit('https://market.drapenihavet.no/mobile.php?barcode=test');
-        // cy.url().should('include', 'boxtribute-production.eu.auth0.com');
+        cy.request({
+            url: 'https://market.drapenihavet.no/mobile.php?barcode=test',
+            followRedirect: false, // turn off following redirects
+          }).then((resp) => {
+            expect(resp.status).to.eq(301)
+            expect(resp.redirectedToUrl).to.include('app.boxtribute.org');
+          })
     });
 });
