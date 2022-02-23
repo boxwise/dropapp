d0f869e280ef6eabb2382b0ca2f1fb6a7ab007fe
Vahid Bazzaz <vahidh@gmail.com>
2022-02-23 18:47:05 +0330

    create new table for auth0 role mapping

---
+++ db/migrations/20220205084503_add_cms_user_groups_roles.php
@@ -0,0 +1,24 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddCmsUserGroupsRoles extends AbstractMigration
+{
+    public function change()
+    {
+        // create Usergroup Roles table
+        $userGroupsRoles = $this->table('cms_usergroups_roles', ['id' => false]);
+
+        $userGroupsRoles->addColumn('cms_usergroups_id', 'integer', ['signed' => false, 'null' => false])
+            ->addColumn('auth0_role_id', 'string', ['null' => false])
+            ->addColumn('auth0_role_name', 'string', [
+                'null' => false,
+                'limit' => 255,
+            ])
+            ->addIndex(['cms_usergroups_id'], ['unique' => false])
+            ->addIndex(['cms_usergroups_id', 'auth0_role_id'], ['unique' => true])
+            ->addForeignKey('cms_usergroups_id', 'cms_usergroups', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])->save();
+    }
+}
