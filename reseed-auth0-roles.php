a938636b5eed6967da3d1c747b9a209f7a5f260f
Vahid Bazzaz <vahidh@gmail.com>
2022-02-25 10:11:59 +0330

    WIP: update auth0 sync script

--- reseed-auth0-roles.php
+++ reseed-auth0-roles.php
@@ -15,12 +15,12 @@ require_once 'library/core.php';
 
 require_once 'library/constants.php';
 
-$rolesTemplate = ['coordinator', 'warehouse_volunteer', 'free_shop_volunteer', 'library_volunteer', 'label_creation'];
+$rolesTemplate = ['administrator', 'coordinator', 'warehouse_volunteer', 'free_shop_volunteer', 'library_volunteer', 'label_creation'];
 
 $methods = [];
 
 foreach ($rolesToActions as $k => $v) {
-    foreach (array_values($rolesActions[$k]) as $k) {
+    foreach (array_values($rolesToActions[$k]) as $k) {
         $methods[] = $k;
     }
 }
@@ -32,9 +32,11 @@ $methods = array_unique($methods);
 // var_dump($resource);
 // updateResources('5ef3760527b0da00215e6209', $methods);
 
-// $resource = getResource('boxtribute-dev-api');
+$resource = getResource($settings['auth0_api_audience']);
 // var_dump($resource);
+updateResources($resource['id'], $methods);
 
+exit;
 // $role = getRolesByName('authenticated_user');
 
 // $methods = $rolesActions['authenticated_user'];
@@ -51,101 +53,34 @@ $methods = array_unique($methods);
 // $methods = $rolesActions['administrator'];
 // $res = updateRolePermissions($role['id'], 'boxtribute-dev-api', $methods);
 
-// $result = db_query('SELECT
-//                         o.id,
-//                         o.label,
-//                         c.id base_id,
-//                         c.name
-//                     FROM camps c INNER JOIN organisations o ON o.id = c.organisation_id
-//                     WHERE (NOT c.deleted OR c.deleted IS NULL) AND (NOT o.deleted OR o.deleted IS NULL)
-//                     ORDER BY o.id, c.id DESC');
+$result = db_query('SELECT
+                        o.id,
+                        o.label,
+                        c.id base_id,
+                        c.name
+                    FROM camps c INNER JOIN organisations o ON o.id = c.organisation_id
+                    WHERE (NOT c.deleted OR c.deleted IS NULL) AND (NOT o.deleted OR o.deleted IS NULL)
+                    ORDER BY o.id, c.id DESC');
 
-// while ($row = db_fetch($result)) {
-//     var_dump($row);
-//     foreach ($rolesTemplate as $roleName) {
-//         $currentRole = 'base_'.$row['base_id'].'_'.$roleName;
-//         $currentRoleDescription = ucwords($row['label']).' - Base '.$row['base_id'].' ('.$row['name'].') - '.ucwords(preg_replace('/\_/', ' ', $roleName));
-//         $role = getRolesByName($currentRole);
-//         usleep(50000);
-//         if (null === $role) {
-//             $role = createRole($currentRole);
-//             usleep(50000);
-//         }
-//         updateRole($role['id'], $currentRole, $currentRoleDescription);
-//         usleep(50000);
-//         if ($role) {
-//             $methods = $rolesActions[$roleName];
-//             $res = updateRolePermissions($role['id'], 'boxtribute-dev-api', $methods);
-//             usleep(50000);
-//         }
-//     }
-// }
-
-// function createRolesForBaseOld($orgId, $orgName, $baseId, $baseName, array &$rolesToActions, array &$menusToActions)
-// {
-//     $rolesTemplate = [
-//         'Administrator' => ['Administrator'],
-//         'Coordinator' => ['Coordinator'],
-//         'Volunteer' => ['warehouse_volunteer', 'free_shop_volunteer'],
-//         'Warehouse Volunteer' => ['warehouse_volunteer'],
-//         'Free Shop Volunteer' => ['free_shop_volunteer'],
-//         'Library Volunteer' => ['library_volunteer'],
-//         'Label Creation Volunteer' => ['label_creation'],
-//     ];
-//     // $rolesTemplate = ['administrator', 'authenticated_user', 'coordinator', 'warehouse_volunteer', 'free_shop_volunteer', 'library_volunteer', 'label_creation'];
-//     $userGroupIdValues = [];
-//     $functionsIds = [];
-//     $userGroupsRoles = [];
-
-//     foreach ($rolesTemplate as $roleName => $auth0Roles) {
-//         echo $roleName;
-
-//         $userLevel = 1;
-//         $userLevel = (preg_match('/coordinator/', $roleName)) ? 2 : $userLevel;
-//         $userLevel = (preg_match('/.*volunteer/', $roleName)) ? 1 : $userLevel;
-
-//         $userGroupIdValues[] = " (NULL, '{$roleName}', CURRENT_TIME(), ".$_SESSION['user']['id'].", NULL, NULL, '{$orgId}', '{$userLevel}', '0', '0', '0', NULL) ";
-
-//         foreach ($auth0Roles as $auth0Role) {
-//             $currentRole = 'base_'.$baseId.'_'.$auth0Role;
-//             $currentRoleDescription = ucwords($orgName).' - Base '.$baseId.' ('.$baseName.') - '.ucwords(preg_replace('/\_/', ' ', $auth0Role));
-
-//             $userGroupsRoles[$auth0Role][] = [
-//                 'roleName' => $currentRole,
-//                 'roleDescription' => $currentRoleDescription,
-//             ];
-//             $functionsIds[$roleName] = isset($functionsIds[$roleName]) ? $functionsIds[$roleName] : [];
-//             $functionsIds[$roleName] = array_merge($functionsIds[$roleName], getMenusByRole($roleName, $rolesToActions, $menusToActions));
-//         }
-//     }
-
-//     db_transaction(function () use ($userGroupIdValues, $functionsIds, $baseId, $userGroupsRoles) {
-//         db_query('INSERT INTO `cms_usergroups` (`id`, `label`, `created`, `created_by`, `modified`, `modified_by`, `organisation_id`, `userlevel`, `allow_laundry_startcycle`, `allow_laundry_block`, `allow_borrow_adddelete`, `deleted`) VALUES '.implode(', ', $userGroupIdValues).';');
-//         $userGroups = db_array('SELECT * from `cms_usergroups` WHERE id > (LAST_INSERT_ID() - ROW_COUNT()) ORDER BY id ASC;');
-//         $index = 0;
-//         $functionIdValues = [];
-//         $roles = array_values($functionsIds);
-//         $baseUserGroupValues = [];
-//         foreach ($userGroups as $userGroup) {
-//             foreach ($functionsIds[$roles[$index]] as $val) {
-//                 $functionIdValues[] = " ('{$val}', '".$userGroup['id']."') ";
-//             }
-//             $baseUserGroupValues[] = "({$baseId}, '".$userGroup['id']."') ";
-//             ++$index;
-//         }
-//         db_query('INSERT INTO `cms_usergroups_functions` (`cms_functions_id`, `cms_usergroups_id`) VALUES '.implode(', ', $functionIdValues).';');
-//         db_query('INSERT INTO `cms_usergroups_camps` (`camp_id`, `cms_usergroups_id`) VALUES '.implode(', ', $baseUserGroupValues).';');
-//         foreach($userGroupsRoles){
-
-//             $auth0Role = createOrUpdateRoleAndPermission($auth0Role, $currentRole, $currentRoleDescription);
-
-//             db_query('INSERT INTO `cms_usergroups_roles` (`cms_usergroups_id`, `auth0_role_id`, `auth0_role_name`) VALUES '.implode(', ', $functionIdValues).';');
-
-//         }
-//     });
-
-//     // TODO: external service - fast-fail / should be sync
-// }
+while ($row = db_fetch($result)) {
+    foreach ($rolesTemplate as $roleName) {
+        $currentRole = 'base_'.$row['base_id'].'_'.$roleName;
+        $currentRoleDescription = ucwords($row['label']).' - Base '.$row['base_id'].' ('.$row['name'].') - '.ucwords(preg_replace('/\_/', ' ', $roleName));
+        $role = getRolesByName($currentRole);
+        usleep(50000);
+        if (null === $role) {
+            $role = createRole($currentRole);
+            usleep(50000);
+        }
+        updateRole($role['id'], $currentRole, $currentRoleDescription);
+        usleep(50000);
+        if ($role) {
+            $methods = $rolesActions[$roleName];
+            $res = updateRolePermissions($role['id'], $settings['auth0_api_audience'], $methods);
+            usleep(50000);
+        }
+    }
+}
 
 //createRolesForBase('100000003', 'BT', '100000003', 'Developer', $rolesToActions, $menusToActions);
 // $roles = getRolesByBaseIds([1, 3]);
--- reseed-auth0.php
+++ reseed-auth0.php
@@ -12,6 +12,37 @@
     $bypassAuthentication = true;
 
     require_once 'library/core.php';
+
+    $result = db_query('SELECT
+    o.id,
+    o.label,
+    c.id base_id,
+    c.name
+    FROM camps c INNER JOIN organisations o ON o.id = c.organisation_id
+    WHERE (NOT c.deleted OR c.deleted IS NULL) AND (NOT o.deleted OR o.deleted IS NULL)
+    ORDER BY o.id, c.id DESC');
+
+    while ($row = db_fetch($result)) {
+        //var_dump($row);
+        foreach ($rolesTemplate as $roleName) {
+            $currentRole = 'base_'.$row['base_id'].'_'.$roleName;
+            $currentRoleDescription = ucwords($row['label']).' - Base '.$row['base_id'].' ('.$row['name'].') - '.ucwords(preg_replace('/\_/', ' ', $roleName));
+            $role = getRolesByName($currentRole);
+            usleep(50000);
+            if (null === $role) {
+                $role = createRole($currentRole);
+                usleep(50000);
+            }
+            updateRole($role['id'], $currentRole, $currentRoleDescription);
+            usleep(50000);
+            if ($role) {
+                $methods = $rolesActions[$roleName];
+                $res = updateRolePermissions($role['id'], $settings['auth0_api_audience'], $methods);
+                usleep(50000);
+            }
+        }
+    }
+
     $db_users = db_query('SELECT id FROM cms_users;');
     while ($db_user = db_fetch($db_users)) {
         updateAuth0UserFromDb($db_user['id'], $settings['test_pwd']);
