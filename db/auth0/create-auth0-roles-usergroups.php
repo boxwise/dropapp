0a5241489fbdd06eaadd29a8ba229f928e35adea
Vahid Bazzaz <vahidh@gmail.com>
2022-03-28 16:00:19 +0430

    remove extra lines to let cron job run for all dbs - create standard roles in auth0 and also standard usergroups in dropapp

--- db/auth0/create-auth0-roles-usergroups.php
+++ db/auth0/create-auth0-roles-usergroups.php
@@ -1,11 +1,7 @@
 <?php
 
-if (!array_key_exists('HTTP_X_APPENGINE_CRON', $_SERVER) && 'dropapp_dev' != $settings['db_database']) {
+if (!array_key_exists('HTTP_X_APPENGINE_CRON', $_SERVER)) {
     throw new Exception('Not called from AppEngine cron service');
-}
-$permittedDatabases = ['dropapp_dev', 'dropapp_demo', 'dropapp_staging'];
-if (!in_array($settings['db_database'], $permittedDatabases)) {
-    throw new Exception('Not permitting a reset of '.$settings['db_database']);
 }
 
 // update Auth0
