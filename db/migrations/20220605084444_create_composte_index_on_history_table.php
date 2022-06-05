47c0af0de7718be800337804d5fa03763f173ef4
Vahid Bazzaz <vahidh@gmail.com>
2022-06-05 17:08:33 +0430

    create composite index on history table

---
+++ db/migrations/20220605084444_create_composte_index_on_history_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateComposteIndexOnHistoryTable extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('history')
+            ->addIndex(['record_id', 'changedate'], [
+                'name' => 'recordid_changedate_composite_index',
+                'unique' => false,
+            ])
+            ->save()
+        ;
+        $this->table('history')
+            ->addIndex(['tablename', 'record_id', 'changedate'], [
+                'name' => 'tablename_recordid_changedate_composite_index',
+                'unique' => false,
+            ])
+            ->save()
+        ;
+    }
+}
