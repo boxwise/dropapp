37045aaa5da0ef9236df75eac9f72b5b24b0a770
HaGuesto <hans@boxwise.co>
2022-06-05 09:56:56 +0200

    add cron folder in gcloud-entry

--- gcloud-entry.php
+++ gcloud-entry.php
@@ -65,10 +65,10 @@ Tracer::inSpan(
         case '/pdf/idcard.php':
         case '/pdf/qr.php':
         case '/pdf/dryfood.php':
-        case '/reseed-db.php':
-        case '/reseed-auth0.php':
-        case '/reseed-roles-auth0.php':
-        case '/db/auth0/create-auth0-roles-usergroups.php':
+        case '/cron/reseed-db.php':
+        case '/cron/reseed-auth0.php':
+        case '/cron/reseed-roles-auth0.php':
+        case '/cron/create-auth0-roles-usergroups.php':
         case '/fake-error.php':
             require substr($parsedUrl, 1); // trim /
 
