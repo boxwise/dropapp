40545ca5bf478f6ce9174c925c246fc4bfce2b76
HaGuesto <hans@boxtribute.org>
2022-12-20 12:29:50 +0100

    fix assignbox

--- mobile/assignbox.php
+++ mobile/assignbox.php
@@ -4,10 +4,20 @@
 
     // filter out the boxes belongs to thedistribution event
     $data['stock'] = db_array('
-        SELECT s.id, CONCAT(s.box_id," - ",s.items,"x ",IFNULL(p.name,"")," ",IFNULL(g.label,""),IF(s2.label IS NULL,"",CONCAT(" (",s2.label,")"))) AS label
+        SELECT 
+            s.id, 
+            CONCAT(s.box_id," - ",s.items,"x ",IFNULL(p.name,"")," ",IFNULL(g.label,""),IF(s2.label IS NULL,"",CONCAT(" (",s2.label,")"))) AS label
         FROM (stock AS s, products AS p, locations AS l)
         LEFT OUTER JOIN genders AS g ON g.id = p.gender_id
         LEFT OUTER JOIN sizes AS s2 ON s2.id = s.size_id
-        WHERE l.type = "Warehouse" AND (NOT s.deleted OR s.deleted IS NULL) AND s.product_id = p.id AND l.id = s.location_id AND l.camp_id = '.$_SESSION['camp']['id'].' AND l.visible AND l.deleted IS NULL AND (s.qr_id IS NULL) ');
+        WHERE 
+            l.type = "Warehouse" AND 
+            (NOT s.deleted OR s.deleted IS NULL) AND 
+            s.box_state_id NOT IN (2,5,6) AND
+            s.product_id = p.id AND 
+            l.id = s.location_id AND 
+            l.camp_id = '.$_SESSION['camp']['id'].' AND 
+            AND l.deleted IS NULL AND 
+            (s.qr_id IS NULL) ');
 
     $tpl->assign('include', 'mobile_assign.tpl');
