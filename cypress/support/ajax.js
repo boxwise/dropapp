533b152a2138c281741d55ede39cbe08e485f19f
HaGuesto <hans@boxtribute.org>
2022-12-21 14:23:44 +0100

    update cypress ajax function

--- cypress/support/ajax.js
+++ cypress/support/ajax.js
@@ -1,39 +1,26 @@
 
 Cypress.Commands.add("setupAjaxActionHook", () => {
-    // set up a hook to listen for POSTs to the action url
-    cy.server();
     // TODO - could this be a wildcard ajax action to wait for?
-    cy.route("POST", "/?action=**").as('ajaxPostAction');
+    cy.intercept("POST", "/?action=**").as('ajaxPostAction');
 });
 
 Cypress.Commands.add("waitForAjaxAction", (expectedRequest,expectedResponse) => {
     // wait for the expected response
-    cy.wait('@ajaxPostAction')
-        .should('have.property', 'status', 200)
-
-    cy.get('@ajaxPostAction')
-        .then(xhr => {
-            cy.log("Request Body", JSON.stringify(xhr.request.body));
-            cy.log("Response Body", JSON.stringify(xhr.response.body));
-        })
-
-    cy.get('@ajaxPostAction')
-        .its('request.body')
-        .should('contain', expectedRequest)
-    
-    cy.get('@ajaxPostAction')
-        .then(xhr => {
-            expect(xhr.response.body.success).to.equal(true);
-            expect(xhr.response.body.message).to.equal(expectedResponse);
-
-            if (xhr.response.body.redirect) {
-                cy.log('Ajax response will trigger redirect');
-                // may need to call cy.reload() here for cypress to deal
-                // with the client-side redirect randomly reloading the page
-                // but so far doesn't seem to be a problem
-                // possibly because in these scenarios we're immediately
-                // navigating elsewhere
-                cy.reload();
-            }
-    });
+    cy.wait('@ajaxPostAction').then(({id,request,response}) => {
+        cy.log("Request Body", request.body);
+        cy.log("Response Body", response.body);
+        expect(request.body).to.contain(expectedRequest);
+        expect(response.statusCode).to.equal(200);
+        expect(response.body).to.contain('"success":true');
+        expect(response.body).to.contain('"message":"' + expectedResponse + '"');
+        if (response.body.includes('"redirect":true')) {
+            cy.log('Ajax response will trigger redirect');
+            // may need to call cy.reload() here for cypress to deal
+            // with the client-side redirect randomly reloading the page
+            // but so far doesn't seem to be a problem
+            // possibly because in these scenarios we're immediately
+            // navigating elsewhere
+            cy.reload();
+        }
+    })
 });
