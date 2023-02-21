e4dc2598ecaca20c7b5d5e8c55b5950c0162898f
HaGuesto <hans@boxtribute.org>
2023-02-21 20:22:13 +0100

    fix format

--- db/migrations/20230221185417_add_received_on_as_shipment_state.php
+++ db/migrations/20230221185417_add_received_on_as_shipment_state.php
@@ -11,16 +11,16 @@ class AddReceivedOnAsShipmentState extends AbstractMigration
             'null' => true,
             'after' => 'sent_by_id',
         ])
-        ->addColumn('receiving_started_by_id', 'integer', [
-            'null' => true,
-            'signed' => false,
-            'after' => 'receiving_started_on',
-        ])
-        ->addForeignKey('receiving_started_by_id', 'cms_users', 'id', [
-            'delete' => 'SET_NULL', 'update' => 'CASCADE',
-        ])
-        ->addIndex(['receiving_started_by_id'], ['name' => 'shipment_receiving_started_by_id'])
-        ->update()
+            ->addColumn('receiving_started_by_id', 'integer', [
+                'null' => true,
+                'signed' => false,
+                'after' => 'receiving_started_on',
+            ])
+            ->addForeignKey('receiving_started_by_id', 'cms_users', 'id', [
+                'delete' => 'SET_NULL', 'update' => 'CASCADE',
+            ])
+            ->addIndex(['receiving_started_by_id'], ['name' => 'shipment_receiving_started_by_id'])
+            ->update()
         ;
     }
 }
