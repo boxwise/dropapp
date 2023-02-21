69f7bb5078fd06c4dda38eafdc9b850806508a6a
HaGuesto <hans@boxtribute.org>
2023-02-21 20:10:48 +0100

    Add Received on state to shipments table

---
+++ db/migrations/20230221185417_add_received_on_as_shipment_state.php
@@ -0,0 +1,26 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddReceivedOnAsShipmentState extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('shipment');
+        $table->addColumn('receiving_started_on', 'datetime', [
+            'null' => true,
+            'after' => 'sent_by_id',
+        ])
+        ->addColumn('receiving_started_by_id', 'integer', [
+            'null' => true,
+            'signed' => false,
+            'after' => 'receiving_started_on',
+        ])
+        ->addForeignKey('receiving_started_by_id', 'cms_users', 'id', [
+            'delete' => 'SET_NULL', 'update' => 'CASCADE',
+        ])
+        ->addIndex(['receiving_started_by_id'], ['name' => 'shipment_receiving_started_by_id'])
+        ->update()
+        ;
+    }
+}
