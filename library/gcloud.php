40bd8761151435ebc6b98753489cd6f8a4a7e6ca
James Crowley <james.crowley@gmail.com>
2023-02-24 22:49:03 +0000

    Revert "Switch session storage to DataStore (#529)"
    This reverts commit 536d393a00f2210883742b8af766737f0a005da2.

--- library/gcloud.php
+++ library/gcloud.php
@@ -35,11 +35,6 @@ function registerGoogleCloudServices($projectId)
     $hostName = @parse_url('http://'.$_SERVER['HTTP_HOST'], PHP_URL_HOST);
     $version = $settings['version'] ?? '0';
     $settings['upload_dir'] = "gs://{$projectId}.appspot.com/{$hostName}/uploads";
-
-    // configure session storage
-    $datastore = new Google\Cloud\Datastore\DatastoreClient();
-    ini_set('session.save_path', "sessions-{$projectId}-{$hostName}");
-    session_set_save_handler(new Google\Cloud\Datastore\DatastoreSessionHandler($datastore), true);
 }
 
 $googleProjectId = getenv('GOOGLE_CLOUD_PROJECT');
