9a68acc0d11c3d012c789db1c828c1a05a88557c
HaGuesto <hans@boxtribute.org>
2023-03-22 17:06:52 +0100

    add Box State to stock export

--- include/stock_export.php
+++ include/stock_export.php
@@ -15,12 +15,14 @@ if ('' != $export_ids_array[0]) {
 $data = db_query(
     'SELECT 
         boxes.*, 
+        bs.label AS box_state,
         g.label AS gender, 
         p.name AS product, 
         s.label AS size, 
         l.label AS location,
         IF(DATEDIFF(now(),boxes.created) = 1, "1 day", CONCAT(DATEDIFF(now(),boxes.created), " days")) AS boxage
     FROM stock as boxes
+    LEFT OUTER JOIN box_state AS bs ON bs.id = boxes.box_state_id
     LEFT OUTER JOIN products AS p ON p.id = boxes.product_id
     LEFT OUTER JOIN locations AS l ON l.id = boxes.location_id
     LEFT OUTER JOIN genders AS g ON g.id = p.gender_id
@@ -29,6 +31,6 @@ $data = db_query(
     $export_ids_array
 );
 unset($_SESSION['export_ids_stock']);
-$keys = ['box_id' => 'Box number', 'product' => 'Product', 'gender' => 'Gender', 'size' => 'Size', 'location' => 'Location',  'boxage' => 'Age', 'items' => 'Items', 'comments' => 'Comments'];
+$keys = ['box_id' => 'Box number', 'box_state' => 'Box state', 'product' => 'Product', 'gender' => 'Gender', 'size' => 'Size', 'location' => 'Location',  'boxage' => 'Age', 'items' => 'Items', 'comments' => 'Comments', 'created' => 'Created on'];
 
 csvexport($data, 'Stock', $keys);
