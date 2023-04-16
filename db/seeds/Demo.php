7e32ee33ee92c9f3d61b5b261a899534098904aa
HaGuesto <hans@boxtribute.org>
2023-04-16 13:19:25 +0200

    Update Shipments seed

--- db/seeds/Demo.php
+++ db/seeds/Demo.php
@@ -196,6 +196,32 @@ class Demo extends AbstractSeed
         $this->table('people')->insert($people)->save();
         $this->table('tags_relations')->insert($tagrelations)->save();
 
+        //------------------- transfer agreements
+        $this->execute("INSERT INTO `transfer_agreement` (`id`, `source_organisation_id`, `target_organisation_id`, `state`, `type`, `requested_on`, `requested_by`, `accepted_on`, `accepted_by`, `terminated_on`, `terminated_by`, `valid_from`, `valid_until`, `comment`) VALUES
+            (1,1,2,'Accepted','Bidirectional','2023-02-09 17:24:29',8,'2023-02-09 17:30:51',37,NULL,NULL,'2023-02-09 17:24:29',NULL,NULL),
+            (2,1,2,'UnderReview','SendingTo','2023-02-09 17:25:46',8,NULL,NULL,NULL,NULL,'2023-02-09 17:25:46',NULL,NULL),
+            (3,1,2,'Canceled','ReceivingFrom','2023-02-09 17:26:02',8,NULL,NULL,'2023-02-09 17:36:09',8,'2023-02-09 17:26:02',NULL,NULL),
+            (4,2,1,'Rejected','Bidirectional','2023-02-09 17:29:09',37,NULL,NULL,'2023-02-09 17:37:45',8,'2023-02-09 17:29:09',NULL,NULL),
+            (5,1,2,'UnderReview','ReceivingFrom','2023-02-09 17:34:50',8,NULL,NULL,NULL,NULL,'2023-02-09 17:34:50',NULL,NULL);");
+
+        $this->execute('INSERT INTO `transfer_agreement_detail` (`id`, `transfer_agreement_id`, `source_base_id`, `target_base_id`) VALUES
+            (1,1,1,2),
+            (2,1,1,3),
+            (3,1,1,4),
+            (4,2,1,2),
+            (5,3,1,3),
+            (6,4,4,1),
+            (7,5,1,3);');
+
+        $this->execute("INSERT INTO `shipment` (`id`, `transfer_agreement_id`, `source_base_id`, `target_base_id`, `state`, `started_on`, `started_by_id`, `sent_on`, `sent_by_id`, `receiving_started_on`, `receiving_started_by_id`, `completed_on`, `completed_by_id`, `canceled_on`, `canceled_by_id`) VALUES
+            (1,1,1,2,'Preparing','2023-02-09 18:05:57',8,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+            (2,1,1,3,'Canceled','2023-02-09 18:06:02',8,NULL,NULL,NULL,NULL,NULL,NULL,NOW(),8),
+            (3,1,1,3,'Preparing','2023-02-09 18:06:25',8,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+            (4,1,1,4,'Sent','2023-02-09 18:06:25',8,NOW(),8,NULL,NULL,NULL,NULL,NULL,NULL),
+            (5,1,2,1,'Receiving','2023-02-09 18:06:25',18,NOW(),18,NOW(),8,NULL,NULL,NULL,NULL),
+            (6,1,3,1,'Lost','2023-02-09 18:06:25',18,NOW(),18,NULL,NULL,NOW(),8,NULL,NULL),
+            (7,1,3,1,'Completed','2023-02-09 18:06:25',18,NOW(),18,NULL,NULL,NOW(),8,NULL,NULL);");
+
         //------------------- products
         $this->execute("INSERT INTO `products` (`id`,`name`,`category_id`,`gender_id`,`sizegroup_id`,`camp_id`,`value`,`maxperadult`,`maxperchild`,`stockincontainer`,`comments`,`deleted`) VALUES 
 			(1,'Winter Jackets',6,2,1,1,0,0,0,1,'','0000-00-00 00:00:00'),
@@ -1585,29 +1611,5 @@ class Demo extends AbstractSeed
             }
         }
         $this->table('transactions')->insert($transactions)->save();
-
-        //------------------- transfer agreements
-        $this->execute("INSERT INTO `transfer_agreement` (`id`, `source_organisation_id`, `target_organisation_id`, `state`, `type`, `requested_on`, `requested_by`, `accepted_on`, `accepted_by`, `terminated_on`, `terminated_by`, `valid_from`, `valid_until`, `comment`) VALUES
-            (1,1,2,'Accepted','Bidirectional','2023-02-09 17:24:29',8,'2023-02-09 17:30:51',37,NULL,NULL,'2023-02-09 17:24:29',NULL,NULL),
-            (2,1,2,'UnderReview','SendingTo','2023-02-09 17:25:46',8,NULL,NULL,NULL,NULL,'2023-02-09 17:25:46',NULL,NULL),
-            (3,1,2,'Canceled','ReceivingFrom','2023-02-09 17:26:02',8,NULL,NULL,'2023-02-09 17:36:09',8,'2023-02-09 17:26:02',NULL,NULL),
-            (4,2,1,'Rejected','Bidirectional','2023-02-09 17:29:09',37,NULL,NULL,'2023-02-09 17:37:45',8,'2023-02-09 17:29:09',NULL,NULL),
-            (5,1,2,'UnderReview','ReceivingFrom','2023-02-09 17:34:50',8,NULL,NULL,NULL,NULL,'2023-02-09 17:34:50',NULL,NULL);");
-
-        $this->execute('INSERT INTO `transfer_agreement_detail` (`id`, `transfer_agreement_id`, `source_base_id`, `target_base_id`) VALUES
-            (1,1,1,2),
-            (2,1,1,3),
-            (3,1,1,4),
-            (4,2,1,2),
-            (5,3,1,3),
-            (6,4,4,1),
-            (7,5,1,3);');
-
-        $this->execute("INSERT INTO `shipment` VALUES
-            (1,1,2,1,'Preparing','2023-02-09 18:05:57',8,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-            (2,1,3,1,'Canceled','2023-02-09 18:06:02',8,'2023-02-09 18:07:10',8,NULL,NULL,NULL,NULL,NULL,NULL),
-            (3,1,3,1,'Preparing','2023-02-09 18:06:25',8,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-            (4,4,1,1,'Preparing','2023-02-09 18:08:15',37,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-            (5,4,1,1,'Preparing','2023-02-09 18:08:20',37,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);");
     }
 }
