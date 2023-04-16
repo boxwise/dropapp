36a9b153f096351b0cbf359846461e89c53d692f
HaGuesto <hans@boxtribute.org>
2023-04-16 17:42:35 +0200

    Add source and target size to shipment detail

---
+++ db/migrations/20230416144646_add_size_to_shipment_detail.php
@@ -0,0 +1,30 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddSizeToShipmentDetail extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('shipment_detail');
+        $table
+            ->addColumn('source_size_id', 'integer', [
+                'null' => false,
+                'signed' => false,
+                'after' => 'target_product_id',
+            ])
+            ->addColumn('target_size_id', 'integer', [
+                'null' => true,
+                'signed' => false,
+                'after' => 'source_size_id',
+            ])
+            ->addForeignKey('source_size_id', 'sizes', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])
+            ->addForeignKey('target_size_id', 'sizes', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])
+            ->update()
+        ;
+    }
+}
