40f0cd7bf2a70a0058c2faa3a54d1b9fdcffd3a9
pylipp <beth.aleph@yahoo.de>
2024-04-22 16:39:17 +0200

    Add DB migrations for standard product table

---
+++ db/migrations/20240422163115_add_standard_product_fk.php
@@ -0,0 +1,33 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddStandardProductForeignKey extends AbstractMigration
+{
+    public function up(): void
+    {
+        $this->table('products')
+            ->addColumn('standard_product_id', 'integer', [
+                'null' => true,
+                'default' => null,
+                'signed' => false,
+                'after' => 'deleted',
+            ])
+            ->addForeignKey('standard_product_id', 'standard_product', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])
+            ->addIndex(['standard_product_id'], ['name' => 'products_standard_product_id'])
+            ->save()
+        ;
+    }
+
+    public function down(): void
+    {
+        $this->table('products')
+            ->removeIndex(['standard_product_id'])
+            ->dropForeignKey('standard_product_id')
+            ->removeColumn('standard_product_id')
+            ->save()
+        ;
+    }
+}
