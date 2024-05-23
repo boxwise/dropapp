3d5361d025e8803015176e25ae1b378af6084221
pylipp <beth.aleph@yahoo.de>
2024-05-23 09:40:58 +0200

    Add unique index in products table
    In order to avoid enabling a standard product multiple times
    simultaneously for one base

--- db/migrations/20240422163115_add_standard_product_fk.php
+++ db/migrations/20240422163115_add_standard_product_fk.php
@@ -19,11 +19,27 @@ class AddStandardProductForeignKey extends AbstractMigration
             ->addIndex(['standard_product_id'], ['name' => 'products_standard_product_id'])
             ->save()
         ;
+        # Add virtual column to enable unique composite index on (camp_id,
+        # not_deleted, standard_product_id). Using the `deleted` column alone
+        # doesn't work since MySQL ignores tuples with a NULL value for
+        # uniqueness constraints.
+        # With the following helper column, a standard product cannot be
+        # enabled if already enabled for the base
+        $this->execute('ALTER TABLE products
+            ADD COLUMN not_deleted BOOLEAN
+            AS (IF(deleted IS NULL, 1, NULL)) STORED;')
+        ;
+        $this->table('products')
+            ->addIndex(['camp_id', 'not_deleted', 'standard_product_id'], ['unique' => true])
+            ->save()
+        ;
     }
 
     public function down(): void
     {
         $this->table('products')
+            ->removeIndex(['camp_id', 'not_deleted', 'standard_product_id'])
+            ->removeColumn('not_deleted')
             ->removeIndex(['standard_product_id'])
             ->dropForeignKey('standard_product_id')
             ->removeColumn('standard_product_id')
