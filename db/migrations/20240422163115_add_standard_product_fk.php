5a638f9acb3b5d2cab35ef560d8bafd5e0bbd45b
HaGuesto <hans@boxtribute.org>
2024-05-27 14:22:17 +0200

    rename db migration class to work with filename

--- db/migrations/20240422163115_add_standard_product_fk.php
+++ db/migrations/20240422163115_add_standard_product_fk.php
@@ -2,7 +2,7 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class AddStandardProductForeignKey extends AbstractMigration
+class AddStandardProductFk extends AbstractMigration
 {
     public function up(): void
     {
