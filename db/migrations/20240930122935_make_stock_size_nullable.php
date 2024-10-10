3964f169adfe12eaf26c1f9175de88948d95d449
HaGuesto <hans@boxtribute.org>
2024-10-10 17:21:17 +0200

    fix down migration and make sizeid fields unsigned

--- db/migrations/20240930122935_make_stock_size_nullable.php
+++ db/migrations/20240930122935_make_stock_size_nullable.php
@@ -7,7 +7,7 @@ class MakeStockSizeNullable extends AbstractMigration
     public function up(): void
     {
         $stock = $this->table('stock');
-        $stock->changeColumn('size_id', 'integer', ['null' => true])
+        $stock->changeColumn('size_id', 'integer', ['null' => true, 'signed' => false])
             ->update()
         ;
     }
@@ -15,7 +15,13 @@ class MakeStockSizeNullable extends AbstractMigration
     public function down(): void
     {
         $stock = $this->table('stock');
-        $stock->changeColumn('size_id', 'integer', ['null' => false])
+        $stock->dropForeignKey('size_id')
+            ->update()
+        ;
+        $stock->changeColumn('size_id', 'integer', ['null' => false, 'signed' => false])
+            ->addForeignKey('size_id', 'sizes', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])
             ->update()
         ;
     }
--- db/migrations/20240930123110_make_shipment_detail_source_size_nullable.php
+++ db/migrations/20240930123110_make_shipment_detail_source_size_nullable.php
@@ -7,7 +7,7 @@ class MakeShipmentDetailSourceSizeNullable extends AbstractMigration
     public function up(): void
     {
         $shipment_detail = $this->table('shipment_detail');
-        $shipment_detail->changeColumn('source_size_id', 'integer', ['null' => true])
+        $shipment_detail->changeColumn('source_size_id', 'integer', ['null' => true, 'signed' => false])
             ->update()
         ;
     }
@@ -15,7 +15,13 @@ class MakeShipmentDetailSourceSizeNullable extends AbstractMigration
     public function down(): void
     {
         $shipment_detail = $this->table('shipment_detail');
-        $shipment_detail->changeColumn('source_size_id', 'integer', ['null' => false])
+        $shipment_detail->dropForeignKey('source_size_id')
+            ->update()
+        ;
+        $shipment_detail->changeColumn('source_size_id', 'integer', ['null' => false, 'signed' => false])
+            ->addForeignKey('source_size_id', 'sizes', 'id', [
+                'delete' => 'RESTRICT', 'update' => 'CASCADE',
+            ])
             ->update()
         ;
     }
