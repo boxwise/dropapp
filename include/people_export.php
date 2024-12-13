8ecef77139e2190740d56848c9a1872f11640111
Vahid Bazzaz <vahidh@gmail.com>
2024-12-13 13:24:34 +0330

    add tags to people export

--- include/people_export.php
+++ include/people_export.php
@@ -13,12 +13,33 @@ if ('' != $export_ids_array[0]) {
 
 $result = db_query(
     '
-	SELECT p.*, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), p.date_of_birth)), "%Y")+0 AS age, IF(ISNULL(p.parent_id),p.id,p.parent_id) AS boxtribute_family_id, IF(ISNULL(p.parent_id), TRUE, FALSE) AS familyhead
-	FROM people AS p
-	WHERE camp_id = ? AND (NOT deleted OR deleted IS NULL) '.($_SESSION['export_ids_people'] ? 'AND p.id in ('.$id_pars.')' : ' AND FALSE'),
+    SELECT 
+        p.*, 
+        DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), p.date_of_birth)), "%Y")+0 AS age, 
+        IF(ISNULL(p.parent_id), p.id, p.parent_id) AS boxtribute_family_id, 
+        IF(ISNULL(p.parent_id), TRUE, FALSE) AS familyhead,
+        (
+            SELECT GROUP_CONCAT(t.label) 
+            FROM tags t 
+            INNER JOIN tags_relations tr ON t.id = tr.tag_id 
+            WHERE tr.object_type = "People" AND tr.object_id = p.id
+        ) AS tags
+    FROM people AS p
+    WHERE camp_id = ? AND (NOT deleted OR deleted IS NULL) '.($_SESSION['export_ids_people'] ? 'AND p.id IN ('.$id_pars.')' : ' AND FALSE'),
     $export_ids_array
 );
 unset($_SESSION['export_ids_people']);
-$keys = ['container' => $_SESSION['camp']['familyidentifier'], 'firstname' => 'Firstname', 'lastname' => 'Surname', 'gender' => 'Gender', 'age' => 'Age', 'comments' => 'Comments', 'boxtribute_family_id' => 'Boxtribute Family ID', 'familyhead' => 'Head of Family'];
+
+$keys = [
+    'container' => $_SESSION['camp']['familyidentifier'],
+    'firstname' => 'Firstname',
+    'lastname' => 'Surname',
+    'gender' => 'Gender',
+    'age' => 'Age',
+    'comments' => 'Comments',
+    'boxtribute_family_id' => 'Boxtribute Family ID',
+    'familyhead' => 'Head of Family',
+    'tags' => 'Tags',
+];
 
 csvexport($result, 'Beneficiaries_'.$_SESSION['camp']['name'], $keys);
