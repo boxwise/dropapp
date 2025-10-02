bb78aa248fbc83b46eedfe1b5562c282c65334c4
Philipp Metzner <pmetzner@freenet.de>
2025-10-02 15:52:45 +0200

    Revert some changes

--- cypress.config.js
+++ cypress.config.js
@@ -29,7 +29,7 @@ module.exports = defineConfig({
     successPwdChangeNotif:
       'Within a few minutes you will receive an e-mail with further instructions to reset your password.',
     orgName: 'TestOrganisation',
-    auth0Domain: 'boxtribute-dev.eu.auth0.com',
+    auth0Domain: 'staging-login.boxtribute.org',
   },
   e2e: {
     // We've imported your old cypress plugins here.
