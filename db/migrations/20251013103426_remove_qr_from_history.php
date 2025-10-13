885b8ca6f12928bce2f1400f460443ed3ce48340
Philipp Metzner <pmetzner@freenet.de>
2025-10-13 12:58:26 +0200

    Add DB migration to clean up history table
    This removes the entries
    
    ```
    changes                     |count(id)| max(changedate)
    ----------------------------+---------+
    New QR-code generated       |    42391| -- 2024-06-24 (see below. There are 30 newer log entries since 2025-08-25 through the v2 createQrCode functionality)
    New QR-code generated by pdf|    97198| -- 2024-06-24 (in Feb 2024, the underlying simpleBulkSaveChangeHistory function was incorrectly updated (98be57f2); released in v1.24.0 on 2024-06-25)
    QR code associated to box.  |    62519| -- 2023-12-06 (in Nov 2023, the corresponding mobile functionality was deleted; released in v1.23.4 on 2023-12-08)
    ```

---
+++ db/migrations/20251013103426_remove_qr_from_history.php
@@ -0,0 +1,23 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class RemoveQrFromHistory extends AbstractMigration
+{
+    public function up(): void
+    {
+        // Remove all 140k history entries about QR code creation before 2025 (due
+        // to a bug, they were not logged from June 2024 onwards anyways).
+        // This keeps entries that stem from the v2 createQrCode mutation from
+        // Aug 2025 onwards (these should actually be logged with tablename =
+        // "stock" and changes "New Qr-code assigned by pdf generation.")
+        $deleted_rows = $this->execute('
+DELETE FROM history
+WHERE tablename = "qr"
+AND changedate < "2025-01-01"
+        ');
+        $this->output->writeln('Deleted rows: '.$deleted_rows);
+    }
+
+    public function down() {}
+}
