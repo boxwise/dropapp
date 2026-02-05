8819561f4b8b6c4310489e9d852c9a123e1971ea
Philipp Metzner <pmetzner@freenet.de>
2026-02-05 09:54:28 +0100

    Remove from/to parameters from simpleBulkSaveChangeHistory

--- library/lib/tools.php
+++ library/lib/tools.php
@@ -298,9 +298,8 @@ function simpleSaveChangeHistory($table, $record, $changes, $now = null, $from =
     db_query('INSERT INTO history (tablename, record_id, changes, user_id, ip, changedate, from_int, from_float, to_int, to_float) VALUES (:table,:id,:change,:user_id,:ip,:now, :from_int, :from_float, :to_int, :to_float)', ['table' => $table, 'id' => $record, 'change' => $changes, 'user_id' => $_SESSION['user']['id'], 'ip' => $_SERVER['REMOTE_ADDR'], 'now' => $now, 'from_int' => $from['int'], 'from_float' => $from['float'], 'to_int' => $to['int'], 'to_float' => $to['float']]);
 }
 
-function simpleBulkSaveChangeHistory($table, $records, $changes, $from = [], $to = [])
+function simpleBulkSaveChangeHistory($table, $records, $changes)
 {
-    // from and to variable must be arrays with entry 'int' or 'float'
     if (!db_tableexists('history')) {
         return;
     }
@@ -308,14 +307,14 @@ function simpleBulkSaveChangeHistory($table, $records, $changes, $from = [], $to
     $params = [];
     if (is_iterable($records)) {
         for ($i = 0; $i < sizeof($records); ++$i) {
-            $query .= "(:table{$i},:id{$i},:change{$i},:user_id{$i},:ip{$i},NOW(), :from_int{$i}, :from_float{$i}, :to_int{$i}, :to_float{$i})";
-            $params = array_merge($params, ['table'.$i => $table, 'id'.$i => $records[$i], 'change'.$i => $changes, 'user_id'.$i => $_SESSION['user']['id'], 'ip'.$i => $_SERVER['REMOTE_ADDR'], 'from_int'.$i => $from['int'], 'from_float'.$i => $from['float'], 'to_int'.$i => $to['int'], 'to_float'.$i => $to['float']]);
+            $query .= "(:table{$i},:id{$i},:change{$i},:user_id{$i},:ip{$i},NOW())";
+            $params = array_merge($params, ['table'.$i => $table, 'id'.$i => $records[$i], 'change'.$i => $changes, 'user_id'.$i => $_SESSION['user']['id'], 'ip'.$i => $_SERVER['REMOTE_ADDR']]);
             if ($i !== sizeof($records) - 1) {
                 $query .= ',';
             }
         }
     }
     if (strlen($query) > 0) {
-        db_query("INSERT INTO history (tablename, record_id, changes, user_id, ip, changedate, from_int, from_float, to_int, to_float) VALUES {$query}", $params);
+        db_query("INSERT INTO history (tablename, record_id, changes, user_id, ip, changedate) VALUES {$query}", $params);
     }
 }
