29414952579c4d987339b80fcd2a16464a805128
Philipp Metzner <pmetzner@freenet.de>
2026-02-06 08:49:03 +0100

    Fix deleteprofile

--- library/ajax/deleteprofile.php
+++ library/ajax/deleteprofile.php
@@ -1,14 +1,15 @@
 <?php
 
 db_transaction(function () {
+    $user_id = $_SESSION['user']['id'];
     // Only append .deleted suffix if the email doesn't already have it
     // This prevents double-deletion if the query is somehow executed twice
     // Pattern checks for .deleted.<digits> after the @ symbol (e.g., user@domain.com.deleted.123)
-    db_query('UPDATE cms_users SET deleted = NOW(), email = CONCAT(email,".deleted.",id) WHERE id = :id AND (NOT deleted OR deleted IS NULL) AND email NOT REGEXP "@.*\.deleted\.[0-9]+$"', ['id' => $_POST['cms_user_id']]);
-    updateAuth0UserFromDb($_POST['cms_user_id']);
+    db_query('UPDATE cms_users SET deleted = NOW(), email = CONCAT(email,".deleted.",id) WHERE id = :id AND (NOT deleted OR deleted IS NULL) AND email NOT REGEXP "@.*\.deleted\.[0-9]+$"', ['id' => $user_id]);
+    updateAuth0UserFromDb($user_id);
 });
 
-simpleSaveChangeHistory('cms_users', $_POST['cms_user_id'], 'Record deleted without undelete');
+simpleSaveChangeHistory('cms_users', $user_id, 'Record deleted without undelete');
 // when a user deactive its account we need to ensure that user logged out immediately and then redirected to Auth0 login page
 global $settings;
 logout();
