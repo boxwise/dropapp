e0ec2ca123e32f9612bb43a5851438e849a6a41f
Philipp Metzner <pmetzner@freenet.de>
2026-05-20 17:06:24 +0200

    use-modified-by

--- db/migrations/20260123123229_unassign_deleted_tags.php
+++ db/migrations/20260123123229_unassign_deleted_tags.php
@@ -9,7 +9,7 @@ class UnassignDeletedTags extends AbstractMigration
         $updated_rows = $this->execute('
             UPDATE tags_relations tr
             INNER JOIN tags t ON t.id = tr.tag_id
-            SET tr.deleted_on = t.deleted, tr.deleted_by_id = 1
+            SET tr.deleted_on = t.deleted, tr.deleted_by_id = COALESCE(t.modified_by, 1)
             WHERE tr.deleted_on IS NULL AND t.deleted > 0
         ');
         $this->output->writeln('Updated rows: '.$updated_rows);
